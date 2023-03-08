94d951461685b2534b0353bc9469740e071c33eb
anharu2394 <haruan2394@gmail.com>
2023-03-08 16:11:43 +0900

    ログインページ作成

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <h1>E-コンサルにログイン</h1>
+    </>
+  );
+};
+
+export default Login;
