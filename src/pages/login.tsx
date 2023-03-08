9da4fc5d58a098c8dc285f0f224ab99a6310ee45
anharu2394 <haruan2394@gmail.com>
2023-03-08 20:49:46 +0900

    ログイン画面の見た目を構築

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import { AiFillApple } from 'react-icons/ai';
 
+const TextField = (props: JSX.IntrinsicElements['input']) => {
+  return (
+    <input
+      className="border-text-field-frame h-12 rounded border border-solid w-80 p-4 mb-4"
+      {...props}
+    />
+  );
+};
+
+const GuideLink = ({children}: {children: string}) =>
+{
+    return (
+      <a className="text-guide-link underline text-sm">
+        {children}
+      </a>
+    );
+}
 const Login: NextPage = () => {
   return (
-    <>
-      <h1>E-コンサルにログイン</h1>
-    </>
+    <div className="mt-6 mb-12">
+      <div className="mx-auto flex w-login-container max-w-login-container flex-col items-center border border-solid border-login-container-frame bg-white py-4">
+        <h1 className="my-6 text-center text-2xl">E-コンサルにログイン</h1>
+        <TextField placeholder="メールアドレス" type="email"/>
+        <TextField placeholder="パスワード" type="password" />
+        <div className="mt-2">
+          <GuideLink>新規登録はこちら</GuideLink>
+        </div>
+        <div className="mt-2">
+          <GuideLink>パスワードを忘れた場合はこちら</GuideLink>
+        </div>
+        <div>
+          <button className="my-4 rounded-full bg-primary py-2 px-14 text-white drop-shadow-[0_4px_10px_rgba(92,107,192,.3)]">
+            ログイン
+          </button>
+        </div>
+        <div>
+          <button className="rounded-md border border-solid border-black py-2 px-10 inline-flex items-center mt-6">
+              <AiFillApple className="inline" size="30" />Appleでログイン
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
