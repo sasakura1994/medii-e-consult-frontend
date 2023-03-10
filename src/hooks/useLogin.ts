856b3be05621d343ef07b575adf5b8943723bc43
anharu2394 <haruan2394@gmail.com>
2023-03-10 11:45:22 +0900

    Profileはグローバルステート

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createApiClient } from '@/libs/apiClient';
 import type { ProfileEntityType } from '@/types/entities/profileEntity';
+import { useRecoilState } from 'recoil';
+import { profileState } from '@/globalStates/profileState';
 
 export type UseLoginType = {
   email: string;
@@ -10,7 +12,6 @@ export type UseLoginType = {
   errorMessage: string;
   setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
   login: () => void;
-  isError: boolean;
   profile?: ProfileEntityType | null;
   token: string;
 };
@@ -19,39 +20,39 @@ export const useLogin = (): UseLoginType =>
 {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [ isError, setIsError ] = React.useState( false );
   const [ errorMessage, setErrorMessage ] = React.useState('');
-  const [ profile, setProfile ] = React.useState<ProfileEntityType | null>( null );
+  const [profile, setProfile] =  useRecoilState(profileState);
   const [ token, setToken ] = React.useState( "" );
 
   const login = async () =>
   {
-    const apiClient = createApiClient();
-    const url = `${process.env.ENDPOINT_URL}/api/doctor/login`;
-    const res = await apiClient.post(url, {
-      mail_address: email,
-      password: password,
-    });
+    try {
+      const apiClient = createApiClient();
+      const url = `${process.env.ENDPOINT_URL}/api/doctor/login`;
+      const res = await apiClient.post(url, {
+        mail_address: email,
+        password: password,
+      });
 
-    console.log('res', res.data);
+      console.log('res', res.data);
 
-    if (!res) {
-      setIsError(true);
-      setErrorMessage('エラーが発生しました');
-      return;
+      if (!res) {
+        throw new Error('エラーが発生しました');
+      }
+      if (res.data.code === -1) {
+        throw new Error(res.data.message);
+      }
+
+      localStorage.setItem('token', res.data.jwt_token);
+      setToken(res.data.jwt_token);
+      setProfile((oldValues) => ({
+        ...oldValues,
+        ...res.data.doctor,
+      }));
+    } catch (e: unknown) {
+      const error = e as Error;
+      setErrorMessage(error.message);
     }
-    if ( res.data.code === -1 )
-    {
-      setIsError( true );
-      console.log( res.data.message );
-      setErrorMessage(res.data.message);
-      return;
-    }
-
-    localStorage.setItem('token', res.data.jwt_token);
-    setToken(res.data.jwt_token);
-    setProfile(res.data.doctor)
-
   }
   return {
     email,
@@ -61,7 +62,6 @@ export const useLogin = (): UseLoginType =>
     errorMessage,
     setErrorMessage,
     login,
-    isError,
     profile,
     token
   };
