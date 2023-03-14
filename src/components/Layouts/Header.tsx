39dc455bf1a31f6f508d77681053b5af32a8e33f
Shoichi Namba <alphabrend@gmail.com>
2023-03-14 17:54:03 +0900

    ヘッダロゴをコンポーネント化

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 import styles from './Header.module.scss';
 import { HeaderMenu } from '@/components/Commons/HeaderMenu';
 import { HeaderSubMenu } from '@/components/Commons/HeaderSubMenu';
 import { HeaderContainer } from './HeaderContainer';
+import { HeaderLogo } from './HeaderLogo';
 
 export const Header: React.FC = () => {
   return (
@@ -19,17 +19,7 @@ export const Header: React.FC = () => {
           className={styles.back_btn__img}
         />
       </button>
-      <div className={styles.header__logo}>
-        <Link href="/">
-          <a className={styles.header__logo_link}>
-            <img
-              src="/images/side_logo.svg"
-              alt="Medii E-コンサル"
-              className={styles.header__logo_item}
-            />
-          </a>
-        </Link>
-      </div>
+      <HeaderLogo />
 
       <HeaderMenu />
       <HeaderSubMenu />
--- src/components/Layouts/HeaderSimple.tsx
+++ src/components/Layouts/HeaderSimple.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { HeaderContainer } from './HeaderContainer';
+import { HeaderLogo } from './HeaderLogo';
 
 export const HeaderSimple: React.FC = () => {
-  return <HeaderContainer>シンプルヘッダー</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <HeaderLogo />
+    </HeaderContainer>
+  );
 };
