ef9e7fa89272b4066df0bd219a271dda0eb44aea
anharu2394 <haruan2394@gmail.com>
2023-03-20 18:04:55 +0900

    アカウント登録のモックのファイル作った

---
+++ src/features/register/registerMock.ts
@@ -0,0 +1,15 @@
+import type {
+  ResponseResolver,
+  DefaultBodyType,
+  RestContext,
+  RestRequest,
+  PathParams,
+} from 'msw';
+
+export const registerMock: ResponseResolver<
+  RestRequest<never, PathParams<string>>,
+  RestContext,
+  DefaultBodyType
+> = (req, res, ctx) => {
+  return res(ctx.status(200));
+  };
