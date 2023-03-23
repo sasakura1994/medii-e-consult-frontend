1a9bce383ccd1d6454975748dab13aa254ed41a1
eighty8 <eighty8.me@gmail.com>
2023-03-23 17:55:40 +0900

    プロフィール詳細に取得データ反映

--- src/features/mypages/editProfile/Detail.tsx
+++ src/features/mypages/editProfile/Detail.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { useEditProfile } from './useEditProfile';
+import { useProfile } from './useProfile';
 import { CaptionWithBody } from '@/components/Parts/CaptionWithBody';
 
 export const Detail: React.FC = () => {
-  const { editProfileScreen, openEdit } = useEditProfile();
+  const {
+    profile,
+    email,
+    hospital,
+    editProfileScreen,
+    openEdit,
+    getMedicalSpecialityNameByCode,
+    getPrefectureNameByCode,
+  } = useProfile();
 
   if (!editProfileScreen.isDetailOpen) {
     return null;
@@ -39,7 +47,7 @@ export const Detail: React.FC = () => {
         <div className="flex">
           <CaptionWithBody
             caption="名前"
-            body="蜂谷 庸正"
+            body={`${profile?.last_name} ${profile?.first_name}`}
             classNames={{
               block: 'mb-6 flex-1',
               caption: 'text-[#999999] mb-2',
@@ -47,7 +55,7 @@ export const Detail: React.FC = () => {
           />
           <CaptionWithBody
             caption="名前（よみ）"
-            body="はちや つねまさ"
+            body={`${profile?.last_name_hira} ${profile?.first_name_hira}`}
             classNames={{
               block: 'mb-6 flex-1',
               caption: 'text-[#999999] mb-2',
@@ -57,33 +65,35 @@ export const Detail: React.FC = () => {
 
         <CaptionWithBody
           caption="生年月日"
-          body="1978年 10月 7日"
+          body={`${profile?.birthday_year}年 ${profile?.birthday_month}月 ${profile?.birthday_day}日`}
           classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
         />
 
-        <div>
-          <CaptionWithBody
-            caption="メールアドレス"
-            body="t.hachiya@medii.jp"
-            classNames={{ block: 'mb-2', caption: 'text-[#999999] mb-2' }}
-          />
-          <button
-            type="button"
-            className="inline-block
-                       rounded
-                       border
-                       border-solid
-                       border-[#999999]
-                       bg-white
-                       px-2
-                       py-2
-                       text-sm
-                       leading-none"
-            onClick={() => console.log('メールアドレス編集')}
-          >
-            メールアドレスを変更
-          </button>
-        </div>
+        {email && !email.is_apple && (
+          <div>
+            <CaptionWithBody
+              caption="メールアドレス"
+              body={email.mail_address}
+              classNames={{ block: 'mb-2', caption: 'text-[#999999] mb-2' }}
+            />
+            <button
+              type="button"
+              className="inline-block
+                         rounded
+                         border
+                         border-solid
+                         border-[#999999]
+                         bg-white
+                         px-2
+                         py-2
+                         text-sm
+                         leading-none"
+              onClick={() => console.log('メールアドレス編集')}
+            >
+              メールアドレスを変更
+            </button>
+          </div>
+        )}
       </div>
 
       <div className="mb-10">
@@ -91,31 +101,44 @@ export const Detail: React.FC = () => {
 
         <CaptionWithBody
           caption="医師資格取得年"
-          body="1999 年"
+          body={`${profile?.qualified_year} 年`}
           classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
         />
 
         <CaptionWithBody
           caption="専門科"
-          body="呼吸器内科"
+          body={getMedicalSpecialityNameByCode(profile?.main_speciality)}
           classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
         />
 
-        <CaptionWithBody
-          caption="その他（対応可能な科目）"
-          body="総合内科"
-          classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
-        />
+        <div className="mb-6">
+          <p className="mb-2 text-[#999999]">その他（対応可能な科目）</p>
+          {getMedicalSpecialityNameByCode(profile?.speciality_2) && (
+            <p className="mb-2">
+              {getMedicalSpecialityNameByCode(profile?.speciality_2)}
+            </p>
+          )}
+          {getMedicalSpecialityNameByCode(profile?.speciality_3) && (
+            <p className="mb-2">
+              {getMedicalSpecialityNameByCode(profile?.speciality_3)}
+            </p>
+          )}
+          {getMedicalSpecialityNameByCode(profile?.speciality_4) && (
+            <p className="mb-2">
+              {getMedicalSpecialityNameByCode(profile?.speciality_4)}
+            </p>
+          )}
+        </div>
 
         <CaptionWithBody
           caption="特によく診てきた疾患・領域"
-          body={undefined}
+          body={profile?.expertise}
           classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
         />
 
         <CaptionWithBody
           caption="専門医資格"
-          body={undefined}
+          body={profile?.qualification}
           classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
         />
       </div>
@@ -123,22 +146,30 @@ export const Detail: React.FC = () => {
       <div className="mb-10">
         <h3 className="mb-4 text-primary">■ 所属病院</h3>
 
-        <CaptionWithBody
-          caption="勤務病院の所在地"
-          body="埼玉県"
-          classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
-        />
+        {(profile?.hospital_id || profile?.hospital_name) && (
+          <>
+            <CaptionWithBody
+              caption="勤務病院の所在地"
+              body={getPrefectureNameByCode(profile?.prefecture_code)}
+              classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
+            />
 
-        <CaptionWithBody
-          caption="勤務先病院名"
-          body="TMG"
-          classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
-        />
+            <CaptionWithBody
+              caption="勤務先病院名"
+              body={hospital?.hospital_name}
+              classNames={{ block: 'mb-6', caption: 'text-[#999999] mb-2' }}
+            />
+          </>
+        )}
       </div>
 
       <div className="mb-10">
         <h3 className="mb-4 text-primary">■ E-コンサル利用区分</h3>
-        <p>回答医&相談（E−コンサルへの回答も行います）</p>
+        <p>
+          {profile?.want_to_be_consultant
+            ? '回答医&相談（E−コンサルへの回答も行います）'
+            : '相談医'}
+        </p>
       </div>
     </>
   );
