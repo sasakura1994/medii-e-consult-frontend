7b9040b4aec94173c8e49ac63ee32fdae13f5314
anharu2394 <haruan2394@gmail.com>
2023-03-30 23:55:56 +0900

    useFetchLatestSeminar追加

---
+++ src/hooks/api/seminar/useFetchLatestSeminar.ts
@@ -0,0 +1,25 @@
+import { useAuthenticatedSWR } from '@/hooks/network/useAuthenticatedSWR';
+import { SeminarEntityType } from '@/types/entities/seminarEntity';
+import type { AxiosError } from 'axios';
+
+export type UseFetchLatestSeminarType = {
+  isLoading: boolean;
+  error: AxiosError | undefined;
+  latestSeminar: SeminarEntityType | undefined;
+};
+
+const endpoint = '/api/seminar/latest';
+
+export const useFetchLatestSeminar = (): UseFetchLatestSeminarType => {
+  const {
+    isLoading,
+    error,
+    data: latestSeminar,
+  } = useAuthenticatedSWR<SeminarEntityType>(endpoint);
+
+  return {
+    isLoading,
+    error,
+    latestSeminar,
+  };
+};
