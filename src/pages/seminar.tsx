732ebf1317b4150c294c1de7618465a273d5cdfc
anharu2394 <haruan2394@gmail.com>
2023-04-11 17:36:52 +0900

    md: to lg:

--- src/pages/seminar.tsx
+++ src/pages/seminar.tsx
@@ -43,54 +43,54 @@ const Seminar: NextPage = () => {
   const { seminars, latestSeminar, ticketCount } = useSeminar();
   const [showModal, setShowModal] = React.useState(false);
   return (
-    <div className="bg-[url('/images/seminar/SP_back.png')] bg-cover bg-no-repeat pb-12 md:bg-[url('/images/seminar/PC_back.png')]">
+    <div className="bg-[url('/images/seminar/SP_back.png')] bg-cover bg-no-repeat pb-12 lg:bg-[url('/images/seminar/PC_back.png')]">
       <div className="m-auto flex max-w-[960px] flex-col items-center py-4 pt-10">
         <h2
           className="h-32  w-72 bg-contain bg-no-repeat px-12 pt-0 text-center text-lg text-[#6c6c6c] 
-        md:bg-[url('/images/seminar/heading_fukidashi.svg')] md:pt-6 md:text-3xl"
+        lg:bg-[url('/images/seminar/heading_fukidashi.svg')] lg:pt-6 lg:text-3xl"
         >
           最新セミナー
         </h2>
-        <div className="mt-30 relative w-full bg-white pt-24 pb-20  md:mt-64 md:w-[960px] md:rounded-lg md:pt-40 md:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
-          <div className="absolute top-[-100px] flex w-full justify-center px-6  md:top-[-264px]">
+        <div className="mt-30 relative w-full bg-white pt-24 pb-20  lg:mt-64 lg:w-[960px] lg:rounded-lg lg:pt-40 lg:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+          <div className="absolute top-[-100px] flex w-full justify-center px-6  lg:top-[-264px]">
             <img
               src={latestSeminar && latestSeminar.image_url}
-              className="aspect-video max-h-[405px] w-full max-w-none drop-shadow-[0_4px_4px_rgba(0,0,0,.25)] md:w-[719px] md:shadow-[20px_27px_0_0_rgb(221,221,221)] md:drop-shadow-none"
+              className="aspect-video max-h-[405px] w-full max-w-none drop-shadow-[0_4px_4px_rgba(0,0,0,.25)] lg:w-[719px] lg:shadow-[20px_27px_0_0_rgb(221,221,221)] lg:drop-shadow-none"
             />
           </div>
-          <div className="flex flex-col items-center bg-white px-8 md:px-32">
-            <a className="w-full md:w-auto">
-              <button className="relative mb-2 mt-6 w-full rounded-lg bg-primary py-3 text-base font-bold text-white md:w-auto md:py-4 md:px-12 md:text-2xl">
+          <div className="flex flex-col items-center bg-white px-8 lg:px-32">
+            <a className="w-full lg:w-auto">
+              <button className="relative mb-2 mt-6 w-full rounded-lg bg-primary py-3 text-base font-bold text-white lg:w-auto lg:py-4 lg:px-12 lg:text-2xl">
                 ZOOMセミナーへ
                 <img
                   src="/images/seminar/main_button_arrow.svg"
-                  className="absolute right-4 top-3 ml-2 inline h-6 md:static  md:ml-6"
+                  className="absolute right-4 top-3 ml-2 inline h-6 lg:static  lg:ml-6"
                 />
               </button>
             </a>
             <a href="/seminar#archive">
-              <button className="mt-2 mb-4 rounded-lg border border-[#7acadc] bg-[#f2f9ff] py-4 px-14 text-primary md:py-5">
+              <button className="mt-2 mb-4 rounded-lg border border-[#7acadc] bg-[#f2f9ff] py-4 px-14 text-primary lg:py-5">
                 過去のセミナー動画はこちら
               </button>
             </a>
             <div className="flex w-full flex-col items-center">
               <div className="flex w-full items-center justify-center rounded-md bg-[#e2e7ff] py-4">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary md:h-20 md:w-20">
+                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary lg:h-20 lg:w-20">
                   <img
                     src="/images/seminar/video.svg"
-                    className="w-[19px] md:w-auto"
+                    className="w-[19px] lg:w-auto"
                   />
                 </div>
-                <p className="pl-4 font-bold text-primary md:text-xl">
+                <p className="pl-4 font-bold text-primary lg:text-xl">
                   {latestSeminar?.subject}
                 </p>
               </div>
             </div>
             <div className="w-full  text-lg">
-              <div className="flex-reverse flex flex-col flex-col-reverse items-center justify-between border-b py-4 md:flex-row">
-                <div className="w-full md:w-auto">
-                  <p className="text-base md:text-lg">
-                    <span className="pr-4 text-2xl text-primary md:text-lg">
+              <div className="flex-reverse flex flex-col flex-col-reverse items-center justify-between border-b py-4 lg:flex-row">
+                <div className="w-full lg:w-auto">
+                  <p className="text-base lg:text-lg">
+                    <span className="pr-4 text-2xl text-primary lg:text-lg">
                       日時
                     </span>
                     {latestSeminar !== undefined
@@ -117,8 +117,8 @@ const Seminar: NextPage = () => {
               </div>
               <div className="border-b py-4">
                 <div>
-                  <p className="text-base md:text-lg">
-                    <span className="pr-4 text-2xl text-primary md:text-lg md:font-bold">
+                  <p className="text-base lg:text-lg">
+                    <span className="pr-4 text-2xl text-primary lg:text-lg lg:font-bold">
                       講師
                     </span>{' '}
                     {latestSeminar?.doctor_name !== undefined
@@ -130,8 +130,8 @@ const Seminar: NextPage = () => {
               </div>
               <div className="pt-4">
                 <div>
-                  <p className="text-base md:text-lg">
-                    <span className="pr-4 text-2xl text-primary md:text-lg">
+                  <p className="text-base lg:text-lg">
+                    <span className="pr-4 text-2xl text-primary lg:text-lg">
                       セミナー概要
                     </span>
                     <br />
@@ -146,26 +146,26 @@ const Seminar: NextPage = () => {
         </div>
         <div
           id="archive"
-          className="flex w-full flex-col items-center rounded-lg px-7 pb-20 md:mt-10 md:max-w-[960px] md:bg-white md:px-20 md:pt-20 md:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
+          className="flex w-full flex-col items-center rounded-lg px-7 pb-20 lg:mt-10 lg:max-w-[960px] lg:bg-white lg:px-20 lg:pt-20 lg:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
         >
-          <h2 className="max-w-[460px] bg-contain bg-no-repeat py-8 text-center text-lg text-[#6c6c6c] md:h-32 md:bg-[url('/images/seminar/list_fukidashi.svg')] md:px-14 md:pt-6 md:pb-0 md:text-3xl">
+          <h2 className="max-w-[460px] bg-contain bg-no-repeat py-8 text-center text-lg text-[#6c6c6c] lg:h-32 lg:bg-[url('/images/seminar/list_fukidashi.svg')] lg:px-14 lg:pt-6 lg:pb-0 lg:text-3xl">
             セミナーアーカイブ動画
           </h2>
-          <div className="mb-6 flex flex-col items-center justify-center md:flex-row">
+          <div className="mb-6 flex flex-col items-center justify-center lg:flex-row">
             <div>
               <div className="relative">
-                <div className="absolute top-0 left-0 right-0 bottom-0 rounded-xl border border-[#c4c4c4] bg-white blur-xs md:rounded-3xl"></div>
+                <div className="absolute top-0 left-0 right-0 bottom-0 rounded-xl border border-[#c4c4c4] bg-white blur-xs lg:rounded-3xl"></div>
                 <div className="relative flex items-center p-6">
                   <img src="/images/seminar/ticket.png" />
-                  <p className="hidden pl-2 md:block md:text-2xl">
+                  <p className="hidden pl-2 lg:block lg:text-2xl">
                     現在のチケット所持枚数
                     <span className="pl-2 text-4xl font-bold text-[#f5847d]">
                       {ticketCount?.ticket_count}
                       <span className="text-lg">枚</span>
                     </span>
                   </p>
-                  <div className="flex md:hidden">
-                    <p className="pl-2 pr-2 md:text-2xl">
+                  <div className="flex lg:hidden">
+                    <p className="pl-2 pr-2 lg:text-2xl">
                       現在のチケット所持
                       <br />
                       枚数
@@ -180,13 +180,13 @@ const Seminar: NextPage = () => {
             </div>
             <button
               onClick={() => setShowModal(true)}
-              className="text-xm mt-4 w-full rounded-md bg-primary py-4 px-4 text-white md:ml-10 md:mt-0 md:w-auto"
+              className="text-xm mt-4 w-full rounded-md bg-primary py-4 px-4 text-white lg:ml-10 lg:mt-0 lg:w-auto"
             >
               チケットを獲得するには?
             </button>
           </div>
           <div>
-            <div className="grid grid-cols-[1fr] gap-4 md:grid-cols-[1fr_1fr]">
+            <div className="grid grid-cols-[1fr] gap-4 lg:grid-cols-[1fr_1fr]">
               {seminars?.slice(0, 2).map((seminar) => (
                 <SeminarCard seminar={seminar} key={seminar.seminar_id} />
               ))}
@@ -194,7 +194,7 @@ const Seminar: NextPage = () => {
           </div>
           <a
             href="/seminar/archives"
-            className="my-6 inline-flex items-center rounded-full border border-primary bg-white px-8 py-2 text-[11px] text-primary md:my-10 md:px-6 md:py-3 md:text-sm"
+            className="my-6 inline-flex items-center rounded-full border border-primary bg-white px-8 py-2 text-[11px] text-primary lg:my-10 lg:px-6 lg:py-3 lg:text-sm"
           >
             すべてのアーカイブ動画を見る
             <img src="/icons/arrow_right.svg" className="ml-2 inline h-3 " />
@@ -205,12 +205,12 @@ const Seminar: NextPage = () => {
         </div>
       </div>
       {showModal && (
-        <Modal setShowModal={setShowModal} className="md:w-[800px]">
-          <div className="align-center relative flex flex-col items-center bg-white px-6 py-4 md:py-20 md:px-28">
+        <Modal setShowModal={setShowModal} className="lg:w-[800px]">
+          <div className="align-center relative flex flex-col items-center bg-white px-6 py-4 lg:lgpy-20 lg:px-28">
             <img
               onClick={() => setShowModal(false)}
               src="/icons/close_primary.svg"
-              className="absolute right-4 top-4 md:right-10 md:top-14"
+              className="absolute right-4 top-4 lg:right-10 lg:top-14"
             />
             <h3 className="text-center text-2xl text-primary">チケットとは?</h3>
             <p className="py-10">
