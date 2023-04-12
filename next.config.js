0ce00fedfbe979d9662bae0806ecf6e418405212
sasakura1994 <kanazawa01211123@gmail.com>
2023-04-12 11:24:58 +0900

    next.config.jsの無駄なスペースの削除

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
 
+// local環境の時はCORS対策のためにWEB_SERVER_URLへのリクエストをENDPOINT_URLへリバースプロキシする
 const rewrites = [
   {
     source: '/api/:path*',
-    destination: `${process.env. ENDPOINT_URL}/api/:path*`,
+    destination: `${process.env.ENDPOINT_URL}/api/:path*`,
   },
 ];
+
 
 const nextConfig = {
   reactStrictMode: true,
   env: {
     WEB_SERVER_URL: process.env.WEB_SERVER_URL,
-    ENDPOINT_URL: process.env.WEB_SERVER_URL,
+    ENDPOINT_URL: process.env.WEB_SERVER_URL
   },
   async rewrites() {
-    return rewrites;
+    return rewrites
   },
 };
+
+module.exports = nextConfig;
