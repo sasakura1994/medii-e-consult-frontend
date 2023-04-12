52eb2d41c95c1ab0d52f9be9ec0545082da9472e
sasakura1994 <kanazawa01211123@gmail.com>
2023-04-12 11:11:42 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const rewrites = [
+  {
+    source: '/api/:path*',
+    destination: `${process.env. ENDPOINT_URL}/api/:path*`,
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
     WEB_SERVER_URL: process.env.WEB_SERVER_URL,
-    ENDPOINT_URL: process.env.ENDPOINT_URL,
+    ENDPOINT_URL: process.env.WEB_SERVER_URL,
+  },
+  async rewrites() {
+    return rewrites;
   },
 };
-
-module.exports = nextConfig;
