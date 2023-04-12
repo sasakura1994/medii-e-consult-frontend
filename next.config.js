1105c6f02e05370d618169f4aa15f67646797b6c
sasakura1994 <kanazawa01211123@gmail.com>
2023-04-12 17:25:59 +0900

    プロキシテスト

--- next.config.js
+++ next.config.js
@@ -1,22 +1,28 @@
 /** @type {import('next').NextConfig} */
 
-const rewrites = process.env.IS_AMPLIFY ? [
-  {
-    source: `/api/:path*`,
-    destination: `${process.env.ENDPOINT_URL}/:path*`,
-  },
-] : [];
+const rewrites =
+  process.env.IS_AMPLIFY === 'true' || process.env.NODE_ENV === 'development'
+    ? [
+        {
+          source: `/api/:path*`,
+          destination: `${process.env.ENDPOINT_URL}/:path*`,
+        },
+      ]
+    : [];
 
 const nextConfig = {
   reactStrictMode: true,
   env: {
     WEB_SERVER_URL: process.env.WEB_SERVER_URL,
     // local環境の時はWEB_SERVER_URLとENDPOINT_URLに指定するが、それ以外の環境ではENDPOINT_URLをそのまま使う
-    ENDPOINT_URL: process.env.NODE_ENV === 'development' ? process.env.WEB_SERVER_URL : process.env.ENDPOINT_URL,
+    ENDPOINT_URL:
+    process.env.IS_AMPLIFY === 'true' || process.env.NODE_ENV === 'development'
+        ? process.env.WEB_SERVER_URL
+        : process.env.ENDPOINT_URL,
   },
   async rewrites() {
     // 現状"/api"で始まってしまっているものが多いので、"/api"が来たら"/api"を削除するようにしている
-    return rewrites
+    return rewrites;
   },
 };
 
