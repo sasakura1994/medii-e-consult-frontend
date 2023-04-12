a67429f9dcbcf3059a90e1863a64f11758984417
sasakura1994 <kanazawa01211123@gmail.com>
2023-04-12 12:29:00 +0900

    add IS_AMPLIFY

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 // local環境の時はCORS対策のためにWEB_SERVER_URLへのリクエストをENDPOINT_URLへリバースプロキシする
-const rewrites = process.env.NODE_ENV === 'development' ? [
+const rewrites = process.env.NODE_ENV === 'development'|| process.env.IS_AMPLIFY ?[
   {
     source: '/api/:path*',
     destination: `${process.env.ENDPOINT_URL}/api/:path*`,
@@ -13,7 +13,7 @@ const nextConfig = {
   env: {
     WEB_SERVER_URL: process.env.WEB_SERVER_URL,
     // local環境の時はWEB_SERVER_URLとENDPOINT_URLに指定するが、それ以外の環境ではENDPOINT_URLをそのまま使う
-    ENDPOINT_URL: process.env.NODE_ENV === 'development' ? process.env.WEB_SERVER_URL : process.env.ENDPOINT_URL,
+    ENDPOINT_URL: process.env.NODE_ENV === 'development' || process.env.IS_AMPLIFY ? process.env.WEB_SERVER_URL : process.env.ENDPOINT_URL,
   },
   async rewrites() {
     return rewrites
