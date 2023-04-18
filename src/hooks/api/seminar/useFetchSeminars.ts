9cea4814a9874446a8666e5f82cf1987c22807fd
anharu2394 <haruan2394@gmail.com>
2023-04-18 13:14:07 +0900

    maxPage allItemsCountを取得

--- src/hooks/api/seminar/useFetchSeminars.ts
+++ src/hooks/api/seminar/useFetchSeminars.ts
@@ -6,18 +6,26 @@ export type UseFetchSeminarsType = {
   isLoading: boolean;
   error: AxiosError | undefined;
   seminars: SeminarEntityType[] | undefined;
+  maxPage: number | undefined;
+  allItemsCount: number | undefined;
 };
 
-const endpoint = '/seminar';
+const endpoint = '/seminar?page=';
 
-export const useFetchSeminars = (): UseFetchSeminarsType => {
+export const useFetchSeminars = ( currentPage ?: number): UseFetchSeminarsType => {
   const { isLoading, error, data } = useAuthenticatedSWR<{
     seminars: SeminarEntityType[];
-  }>(endpoint);
+    max_page: number;
+    all_items_count: number;
+  }>(endpoint + (currentPage ?? 1));
   const seminars = data?.seminars;
+  const maxPage = data?.max_page;
+  const allItemsCount = data?.all_items_count;
   return {
     isLoading,
     error,
     seminars,
+    maxPage,
+    allItemsCount,
   };
 };
