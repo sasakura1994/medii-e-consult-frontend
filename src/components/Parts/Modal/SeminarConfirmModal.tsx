a792ca6fe0c87d977621eba2b31a5fe884ae3972
anharu2394 <haruan2394@gmail.com>
2023-04-24 17:04:47 +0900

    SeminarConfirmModalを作った

---
+++ src/components/Parts/Modal/SeminarConfirmModal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Modal } from './Modal';
+import { PrimaryButton } from '../Button/PrimaryButton';
+
+export type ModalPropsType = {
+  children: React.ReactNode;
+  className?: string;
+  setShowModal: (isShow: boolean) => void;
+  onSubmit?: () => void;
+  title: string;
+  labelText: string;
+};
+
+export const SeminarConfirmModal: React.FC<ModalPropsType> = ( props ) =>
+{
+  const { children, className, setShowModal, onSubmit, title, labelText, } = props;
+
+  return (
+    <Modal setShowModal={ setShowModal } className={`lg:w-[800px] ${className}`}>
+      <div>
+        <h1>{ title }</h1>
+        {children}
+        <div>
+          <PrimaryButton>キャンセル</PrimaryButton>
+          <PrimaryButton onClick={ () => onSubmit && onSubmit() }>{ labelText }</PrimaryButton>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
