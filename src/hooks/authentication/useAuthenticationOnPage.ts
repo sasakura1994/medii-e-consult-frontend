f59f0732f9da64bb2ea42a5b95f231ec77182ff0
Shoichi Namba <alphabrend@gmail.com>
2023-05-02 16:23:52 +0900

    コメント追記

--- src/hooks/authentication/useAuthenticationOnPage.ts
+++ src/hooks/authentication/useAuthenticationOnPage.ts
@@ -2,6 +2,9 @@ import React from 'react';
 import { useToken } from './useToken';
 import { redirectToLoginPage } from '@/libs/apiClient';
 
+/**
+ * 認証が必要なページに配置する
+ */
 export const useAuthenticationOnPage = () => {
   const { token, isTokenInitialized } = useToken();
 
