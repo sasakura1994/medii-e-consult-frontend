c934e09cabecf078fa7a1cdf54db1ad18e3c25a3
sasakura1994 <kanazawa01211123@gmail.com>
2023-05-15 11:54:22 +0900

    医師資格確認資料登録 - 医師番号を入力画面の作成 処理部分のカスタムhook化を行っていなかったので、切り出しを行った

---
+++ src/features/document/useDoctorNumberForm.ts
@@ -0,0 +1,122 @@
+import { useUploadDocument } from '@/hooks/api/doctor/useUploadDocument';
+import { Era, useEraConverter } from '@/hooks/useEraConverter';
+import { useProfile } from '@/hooks/useProfile';
+import { useState, useCallback, useEffect, useMemo } from 'react';
+import { DocumentSelected } from '.';
+
+type UseDoctorNumberFormProps = {
+  setSelected: React.Dispatch<React.SetStateAction<DocumentSelected>>;
+};
+
+type UseDoctorNumberForm = {
+  doctorNumber: string;
+  setDoctorNumber: React.Dispatch<React.SetStateAction<string>>;
+  errorMessage: string;
+  isUpdatePrepared: boolean;
+  submit: () => Promise<void>;
+  validation: {
+    min: number;
+    max: number;
+  };
+  handleEraChange: (value: string) => void;
+  inputYear: string;
+  doctorLicenseMonth: string;
+  setDoctorLicenseMonth: React.Dispatch<React.SetStateAction<string>>;
+  doctorLicenseDay: string;
+  setDoctorLicenseDay: React.Dispatch<React.SetStateAction<string>>;
+  handleInputYearToSeireki: (value: string) => void;
+  handleDoctorLicenseYearToJapaneseEraYear: (currentEra: Era) => void;
+};
+
+export const useDoctorNumberForm = ({
+  setSelected,
+}: UseDoctorNumberFormProps): UseDoctorNumberForm => {
+  const { profile } = useProfile();
+
+  const { uploadDocument } = useUploadDocument();
+  const [errorMessage, setErrorMessage] = useState('');
+  const [doctorNumber, setDoctorNumber] = useState('');
+  const [doctorLicenseYear, setDoctorLicenseYear] = useState('');
+  const [doctorLicenseMonth, setDoctorLicenseMonth] = useState('');
+  const [doctorLicenseDay, setDoctorLicenseDay] = useState('');
+  const {
+    inputYear,
+    convertYear,
+    era,
+    setInputYear,
+    validation,
+    handleEraChange,
+  } = useEraConverter();
+  const isUpdatePrepared = useMemo(() => {
+    if (
+      doctorNumber &&
+      doctorLicenseYear &&
+      doctorLicenseMonth &&
+      doctorLicenseDay
+    ) {
+      return true;
+    }
+    return false;
+  }, [doctorNumber, doctorLicenseYear, doctorLicenseMonth, doctorLicenseDay]);
+
+  const handleInputYearToSeireki = useCallback(
+    (value: string) => {
+      setInputYear(value);
+      const year = convertYear(value, era, 'year');
+      setDoctorLicenseYear(year.toString());
+    },
+    [convertYear, era, setInputYear]
+  );
+
+  const handleDoctorLicenseYearToJapaneseEraYear = useCallback(
+    (currentEra: Era) => {
+      const newYear = convertYear(doctorLicenseYear, 'year', currentEra);
+
+      setInputYear(newYear);
+    },
+    [convertYear, doctorLicenseYear, setInputYear]
+  );
+
+  const submit = async () => {
+    if (profile) {
+      const year = convertYear(inputYear, era, 'year');
+      const newProfile = { ...profile };
+      newProfile.doctor_number = doctorNumber;
+      newProfile.doctor_qualified_year = Number(year);
+      newProfile.doctor_qualified_month = Number(doctorLicenseMonth);
+      newProfile.doctor_qualified_day = Number(doctorLicenseDay);
+      newProfile.confimation_type = 'number';
+      await uploadDocument(newProfile).catch((e) => {
+        setErrorMessage(e.message);
+      });
+      setSelected('completed');
+    }
+  };
+
+  useEffect(() => {
+    if (profile) {
+      setDoctorNumber(profile.doctor_number);
+      setDoctorLicenseYear(profile.doctor_qualified_year.toString());
+      setInputYear(profile.doctor_qualified_year.toString());
+      setDoctorLicenseMonth(profile.doctor_qualified_month.toString());
+      setDoctorLicenseDay(profile.doctor_qualified_day.toString());
+    }
+  }, [profile, setInputYear]);
+
+  return {
+    doctorNumber,
+    setDoctorNumber,
+    errorMessage,
+    isUpdatePrepared,
+    submit,
+    validation,
+    handleEraChange,
+    inputYear,
+    doctorLicenseMonth,
+    setDoctorLicenseMonth,
+    doctorLicenseDay,
+    setDoctorLicenseDay,
+    handleInputYearToSeireki,
+    handleDoctorLicenseYearToJapaneseEraYear,
+  };
+};
