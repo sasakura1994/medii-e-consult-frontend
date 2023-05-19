dc12a334d561a56d6412ba01c37f0133d7bf1d3f
sasakura1994 <kanazawa01211123@gmail.com>
2023-05-19 14:34:01 +0900

    シンプルに使えそうなpathを書き出してみる

--- next.config.js
+++ next.config.js
@@ -2,32 +2,75 @@
 
 const paths = [
   '/Top',
+  '/top',
   "/Assign",
+  "/assign",
   "/Seminar/Archive",
+  "/Seminar/archive",
+  "/seminar/Archive",
+  "/seminar/archive",
   '/Affiliate',
+  '/affiliate',
   '/AmazonGift',
+  '/amazonGift',
+  '/Amazongift',
+  '/amazongift',
   '/Document',
+  '/document',
   '/EditProfile',
+  '/Editprofile',
+  '/editProfile',
   '/HowToUse',
+  '/howToUse',
+  '/HowtoUse',
+  '/HowTouse',
+  '/Howtouse',
+  '/howtoUse',
+  '/howTouse',
+  '/howtouse',
   '/InitPassword',
+  '/Initpassword',
+  '/initPassword',
   '/Login',
+  '/login',
   '/NewChatRoom',
+  '/NewChatroom',
+  '/NewchatRoom',
+  '/newChatRoom',
+  '/newChatroom',
+  '/Newchatroom',
+  '/newchatRoom',
+  '/newchatroom',
   '/NotifySettings',
+  '/Notifysettings',
+  '/notifySettings',
+  '/notifysettings',
   '/PasswordReset',
+  '/Passwordreset',
+  '/passwordReset',
+  '/passwordreset',
   '/PasswordResetRequest',
+  '/PasswordResetrequest',
+  '/PasswordresetRequest',
+  '/passwordResetRequest',
+  '/passwordresetRequest',
+  '/Passwordresetrequest',
+  '/passwordResetrequest',
+  '/passwordresetrequest',
   '/PointHistory',
+  '/Pointhistory',
+  '/pointHistory',
+  '/pointhistory',
+  '/registration',
   '/Registration',
-];
+]
+
 const result = paths.map((path) => {
   const lowerPath = path.toLowerCase();
-  const reg = new RegExp(path, 'i');
-  const regPath = reg.toString();
-  if (lowerPath !== path) {
     return {
-      source: regPath,
+      source: path,
       destination: lowerPath,
     }
-  }
 });
 
 const nextConfig = {
