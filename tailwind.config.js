018de5efd13e26f815f97c07dbff47a7ac2ee478
sasakura1994 <kanazawa01211123@gmail.com>
2023-05-26 23:06:40 +0900

    `sm`と`xl`は標準のfontSizeと競合してしまい、影響を与えるため、コメントアウト

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
       },
       fontSize: {
         'xxs': '10px',
-        'sm': '12px',
+        // 'sm': '12px',
         'md': '15px',
         'l': '17px',
-        'xl': '19px',
+        // 'xl': '19px',
         'xxl': '21px',
         'xxxl': '28px',
       },
