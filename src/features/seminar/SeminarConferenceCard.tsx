59dfc90b0bbfcedae656d6dc3f91797a15da7d46
sasakura1994 <kanazawa01211123@gmail.com>
2023-06-03 20:02:07 +0900

    不要なコンポーネントの削除

--- src/features/seminar/SeminarConferenceCard.tsx
+++ src/features/seminar/SeminarConferenceCard.tsx
@@ -1,3 +1,4 @@
+import PrimaryButton from '@/components/Button/Primary';
 import SecondaryButton from '@/components/Button/Secondary';
 import TertiaryButton from '@/components/Button/Tertiary';
 import { SeminarEntityType } from '@/types/entities/seminarEntity';
@@ -5,27 +6,102 @@ import React, { useMemo, useState } from 'react';
 
 type SeminarConferenceCardProps = {
   seminar: SeminarEntityType;
+  index: number;
 };
 
 export const SeminarConferenceCard = (props: SeminarConferenceCardProps) => {
-  const { seminar } = props;
+  const { seminar, index } = props;
   const [isOpened, setIsOpened] = useState(false);
+
+  const googleCalendarUrl = useMemo(() => {
+    if (!seminar) return '';
+    const date = seminar.seminar_date.substring(0, 11);
+    const start = encodeURIComponent(
+      (date + seminar.seminar_start_time).replace(/[-:]/g, '')
+    );
+    const end = encodeURIComponent(
+      (date + seminar.seminar_end_time).replace(/[-:]/g, '')
+    );
+    return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
+      seminar.subject
+    )}&dates=${start}/${end}&details=${encodeURIComponent(
+      seminar.description
+    )}&location=${encodeURIComponent(seminar.zoom_url)}`;
+  }, [seminar]);
+
   const dateTime = useMemo(() => {
     const seminarDate = new Date(seminar.seminar_date);
     const time = seminar.seminar_start_time.substring(0, 5);
-    return `${seminarDate.getFullYear()}/${seminarDate.getMonth()}/${seminarDate.getDate()} ${time}`;
+    return `${seminarDate.getFullYear()}/${
+      seminarDate.getMonth() + 1
+    }/${seminarDate.getDate()} ${time}`;
   }, [seminar.seminar_date, seminar.seminar_start_time]);
+  if (index === 0) {
+    return (
+      <div className="mt-6 flex h-auto w-[343px] flex-wrap rounded-lg bg-white p-4 shadow-high lg:w-[928px] lg:p-0">
+        <img
+          alt=""
+          src={seminar.image_url}
+          className="h-[183px] w-[327px] lg:m-4 lg:h-[237px] lg:w-[424px]"
+        />
+        <div className="m-4 ml-0 lg:w-[440px]">
+          <a href={seminar.zoom_url} target="_blank" rel="noreferrer">
+            <PrimaryButton>ZOOMセミナーへ参加</PrimaryButton>
+          </a>
+          <div className="mt-1 flex items-center">
+            <p className="text-md">{dateTime}</p>
+            <div className="ml-5">
+              <a href={googleCalendarUrl} target="_blank" rel="noreferrer">
+                <SecondaryButton>
+                  <div className="mr-1 h-4 w-4 bg-[#D9D9D9]" />
+                  <p>カレンダーに登録</p>
+                </SecondaryButton>
+              </a>
+            </div>
+          </div>
+          <p className="mt-3 text-md font-bold">セミナー概要</p>
+          <p
+            className={
+              !isOpened
+                ? 'mt-1 text-ellipsis text-md line-clamp-3'
+                : 'mt-1 text-md'
+            }
+          >
+            {seminar.description}
+          </p>
+          <div className="mt-2 flex justify-center">
+            <TertiaryButton
+              size="medium"
+              onClick={() => {
+                setIsOpened((prev) => !prev);
+              }}
+            >
+              <p className="text-medii-sm font-bold">
+                {isOpened ? '閉じる' : 'もっと読む'}
+              </p>
+            </TertiaryButton>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="mt-6 h-auto w-[456px] rounded-lg bg-white p-4 shadow-high">
-      <img alt="" className="h-60 w-[440px]" src={seminar.image_url} />
+    <div className="mt-6 mr-4 h-auto w-[343px] rounded-lg bg-white p-4 shadow-high lg:w-[456px]">
+      <img
+        alt=""
+        className="h-[183px] w-[327px] lg:h-[237px] lg:w-[424px]"
+        src={seminar.image_url}
+      />
       <div className="mt-2 h-auto">
         <div className="flex items-center">
           <p className="text-md">{dateTime}</p>
           <div className="ml-5">
-            <SecondaryButton>
-              <div className="mr-1 h-4 w-4 bg-[#D9D9D9]" />
-              <p>カレンダーに登録</p>
-            </SecondaryButton>
+            <a href={googleCalendarUrl} target="_blank" rel="noreferrer">
+              <SecondaryButton>
+                <div className="mr-1 h-4 w-4 bg-[#D9D9D9]" />
+                <p>カレンダーに登録</p>
+              </SecondaryButton>
+            </a>
           </div>
         </div>
         <p className="mt-3 text-md font-bold">セミナー概要</p>
--- src/pages/seminar/index.tsx
+++ src/pages/seminar/index.tsx
@@ -1,113 +1,118 @@
 import React from 'react';
-import type { NextPage } from 'next';
 import { useSeminar } from '@/features/seminar/useSeminar';
 import { SeminarCard } from '@/features/seminar/seminarCard';
 import { Modal } from '@/components/Parts/Modal/Modal';
 import { SeminarArchiveHeader } from '@/features/seminar/seminarArchiveHeader';
 import Link from 'next/link';
 import { useEventLog } from '@/hooks/api/eventLog/useEventLog';
-import { SeminarFirstConferenceCard } from '@/features/seminar/SeminarFirstConferenceCard';
 import { SeminarConferenceCard } from '@/features/seminar/SeminarConferenceCard';
 import SecondaryButton from '@/components/Button/Secondary';
+import { NextPageWithLayout } from '../_app';
 
-const Seminar: NextPage = () => {
+const Seminar: NextPageWithLayout = () => {
   const { seminars, ticketCount } = useSeminar();
   const [showModal, setShowModal] = React.useState(false);
   useEventLog({ name: '/seminar' });
-
-  return (
-    <div
-      className="-mt-10 -mb-20 bg-[url('/images/seminar/SP_back.png')] bg-cover
+  if (seminars) {
+    return (
+      <div
+        className="-mt-10 -mb-20 bg-[url('/images/seminar/SP_back.png')] bg-cover
      bg-no-repeat lg:bg-[url('/images/seminar/PC_back.png')]"
-    >
-      <div className="m-auto flex w-full flex-col items-center py-4 pt-10 lg:w-[960px]">
-        <div className="mb-8 flex h-auto flex-col rounded-lg bg-white px-4 shadow-low lg:max-w-[960px] lg:pt-6">
-          <p className="text-xxxl font-bold text-medii-blue-base">
-            最新のセミナー
-          </p>
-
-          {seminars && <SeminarFirstConferenceCard seminar={seminars[0]} />}
-          <div className="flex flex-col space-x-4 lg:flex-row">
-            {seminars &&
-              seminars.map((seminar, index) => {
-                if (index === 1 || index === 2) {
+      >
+        <div className="flex w-full flex-col items-center justify-center py-4 pt-10">
+          <div className="mb-8 h-auto rounded-lg bg-white pl-6 pt-6 shadow-low lg:max-w-[976px]">
+            <p className="text-xxxl font-bold text-medii-blue-base">
+              最新のセミナー
+            </p>
+            <div className="flex flex-col flex-wrap justify-start lg:flex-row">
+              {seminars.map((seminar, index) => {
+                if (index < 3) {
                   return (
                     <div key={seminar.seminar_id}>
-                      <SeminarConferenceCard seminar={seminar} />
+                      <SeminarConferenceCard index={index} seminar={seminar} />
                     </div>
                   );
                 }
               })}
-          </div>
+            </div>
 
-          <div className="mx-auto my-6 w-44">
-            <SecondaryButton size="large">過去のセミナー動画へ</SecondaryButton>
-          </div>
-        </div>
-
-        <div
-          className="flex w-full flex-col items-center rounded-lg px-7 pb-20
-         lg:mt-4 lg:max-w-[960px] lg:bg-white lg:px-20 lg:pt-10 lg:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
-        >
-          <SeminarArchiveHeader ticketCount={ticketCount} />
-          <div>
-            <div className="grid grid-cols-[1fr] gap-4 lg:grid-cols-[1fr_1fr]">
-              {seminars?.slice(0, 2).map((seminar) => (
-                <SeminarCard seminar={seminar} key={seminar.seminar_id} />
-              ))}
+            <div className="mx-auto my-6 w-44">
+              <Link href={'/seminar/archives'}>
+                <SecondaryButton size="large">
+                  過去のセミナー動画へ
+                </SecondaryButton>
+              </Link>
             </div>
           </div>
-          <Link href="/seminar/archives">
-            <a className="my-6 inline-block lg:my-10">
-              <div
-                className="inline-flex items-center rounded-full border
+
+          <div
+            className="flex w-full flex-col items-center rounded-lg px-7 pb-20
+         lg:mt-4 lg:max-w-[960px] lg:bg-white lg:px-20 lg:pt-10 lg:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
+          >
+            <SeminarArchiveHeader ticketCount={ticketCount} />
+            <div>
+              <div className="grid grid-cols-[1fr] gap-4 lg:grid-cols-[1fr_1fr]">
+                {seminars?.slice(0, 2).map((seminar) => (
+                  <SeminarCard seminar={seminar} key={seminar.seminar_id} />
+                ))}
+              </div>
+            </div>
+            <Link href="/seminar/archives">
+              <a className="my-6 inline-block lg:my-10">
+                <div
+                  className="inline-flex items-center rounded-full border
                border-primary bg-white px-8 py-2 text-[11px] text-primary
              lg:px-6 lg:py-3 lg:text-sm"
-              >
-                すべてのアーカイブ動画を見る
-                <img
-                  src="/icons/arrow_right.svg"
-                  className="ml-2 inline h-3"
-                  alt=""
-                />
-              </div>
-            </a>
-          </Link>
-          <p className="text-center text-sm">
-            ※アーカイブ動画に表示している医師の方々の経歴などの情報は、セミナー当時のものとなっております。
-          </p>
-        </div>
-      </div>
-      {showModal && (
-        <Modal setShowModal={setShowModal} className="lg:w-[800px]">
-          <div className="align-center relative flex flex-col items-center bg-white px-6 py-4 lg:px-28 lg:py-20">
-            <img
-              onClick={() => setShowModal(false)}
-              src="/icons/close_primary.svg"
-              className="absolute right-4 top-4 lg:right-10 lg:top-14"
-              alt=""
-            />
-            <h3 className="text-center text-2xl text-primary">チケットとは?</h3>
-            <p className="py-10">
-              チケットとはセミナー動画アーカイブを閲覧するために必要なものです。
-              <br />
-              E-コンサルで相談するとセミナーチケット1枚獲得できます。
+                >
+                  すべてのアーカイブ動画を見る
+                  <img
+                    src="/icons/arrow_right.svg"
+                    className="ml-2 inline h-3"
+                    alt=""
+                  />
+                </div>
+              </a>
+            </Link>
+            <p className="text-center text-sm">
+              ※アーカイブ動画に表示している医師の方々の経歴などの情報は、セミナー当時のものとなっております。
             </p>
-            <img
-              className="hidden md:block"
-              src="/images/seminar/about_ticket_pc.png"
-              alt=""
-            />
-            <img
-              className="md:hidden"
-              src="/images/seminar/about_ticket_sp.png"
-              alt=""
-            />
           </div>
-        </Modal>
-      )}
-    </div>
-  );
+        </div>
+
+        {showModal && (
+          <Modal setShowModal={setShowModal} className="lg:w-[800px]">
+            <div className="align-center relative flex flex-col items-center bg-white px-6 py-4 lg:px-28 lg:py-20">
+              <img
+                onClick={() => setShowModal(false)}
+                src="/icons/close_primary.svg"
+                className="absolute right-4 top-4 lg:right-10 lg:top-14"
+                alt=""
+              />
+              <h3 className="text-center text-2xl text-primary">
+                チケットとは?
+              </h3>
+              <p className="py-10">
+                チケットとはセミナー動画アーカイブを閲覧するために必要なものです。
+                <br />
+                E-コンサルで相談するとセミナーチケット1枚獲得できます。
+              </p>
+              <img
+                className="hidden md:block"
+                src="/images/seminar/about_ticket_pc.png"
+                alt=""
+              />
+              <img
+                className="md:hidden"
+                src="/images/seminar/about_ticket_sp.png"
+                alt=""
+              />
+            </div>
+          </Modal>
+        )}
+      </div>
+    );
+  }
+  return <></>;
 };
 
 export default Seminar;
