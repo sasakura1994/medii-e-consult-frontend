c80deb5fafe30b0e1a4ed82c72f2183d2ede7d78
anharu2394 <haruan2394@gmail.com>
2023-06-13 12:26:04 +0900

    個別画面の細かい修正

--- src/pages/seminar/[id].tsx
+++ src/pages/seminar/[id].tsx
@@ -50,7 +50,7 @@ const Seminar: NextPage = () => {
   const [, setSwiperRef] = React.useState<SwiperRef | null>(null);
   return (
     <div className="bg-[url('/images/seminar/SP_back.png')] bg-cover bg-no-repeat pb-12 pt-32 lg:bg-[url('/images/seminar/PC_back.png')] lg:pt-10">
-      <div className="relative mx-auto w-full bg-white pb-20 pt-36 lg:max-w-[960px] lg:rounded-2xl lg:bg-white lg:p-10 lg:px-10 lg:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+      <div className="mx-auto lg:max-w-[960px] lg:rounded-2xl lg:bg-white lg:py-10  lg:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
         {seminar?.movie_url ? (
           <div className="absolute top-[-79px] px-[27px] lg:static lg:px-0">
             <iframe
@@ -64,101 +64,111 @@ const Seminar: NextPage = () => {
         ) : (
           <img
             src={seminar && seminar.image_url}
-            className="aspect-video w-full"
+            className="relative z-20 mx-auto -mt-16 aspect-video w-5/6 shadow-lg lg:mt-0 lg:w-full lg:max-w-[960px] lg:bg-white lg:px-10 lg:pt-0 lg:shadow-none"
           />
         )}
-        {!seminar?.movie_url && (
-          <div>
-            <div>
-              <p>チケットを一枚消費して動画を閲覧可能です</p>
-            </div>
-            <div>
-              <p>
-                現在のチケット所持枚数 {ticketCount && ticketCount.ticket_count}{' '}
-                枚
-              </p>
-            </div>
-            <PrimaryButton onClick={() => setIsTicketConfirmDialogShown(true)}>
-              動画を閲覧する
-            </PrimaryButton>
-          </div>
-        )}
-        <div className="flex flex-col items-center bg-white px-7 lg:px-[60px]">
-          <div className="mt-4 flex w-full flex-col items-center">
-            <div className="flex w-full items-center justify-center rounded-md bg-[#e2e7ff] py-4">
-              <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary lg:h-20 lg:w-20">
-                <img
-                  src="/images/seminar/video.svg"
-                  className="w-[19px] lg:w-auto"
-                />
+        <div className="relative mx-auto -mt-24 w-full bg-white pt-28 lg:mt-0 lg:max-w-[960px] lg:rounded-2xl lg:bg-transparent lg:p-10 lg:px-10 lg:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] lg:shadow-none">
+          {!seminar?.movie_url && (
+            <div className="mx-8 mt-4 bg-gray-100 px-2 py-6 lg:mx-0 lg:mt-0">
+              <div className="text-center text-sm font-semibold text-gray-500 lg:text-2xl">
+                チケットを一枚消費して動画を閲覧可能です
               </div>
-              <p className="pl-4 font-bold text-primary lg:text-2xl">
-                {seminar && seminar.subject}
-              </p>
+
+              <div className="mt-2 flex items-end justify-center text-center text-xl font-semibold text-primary lg:text-2xl">
+                現在のチケット所持枚数
+                <p className="mx-2 text-2xl lg:text-4xl">
+                  {ticketCount && ticketCount.ticket_count}
+                </p>
+                <p>枚</p>
+              </div>
+
+              <div className="mt-4 text-center">
+                <PrimaryButton
+                  className="py-2 px-4 lg:px-12 lg:pt-3 lg:pb-4 lg:text-2xl mx-auto"
+                  onClick={() => setIsTicketConfirmDialogShown(true)}
+                >
+                  動画を閲覧する
+                </PrimaryButton>
+              </div>
             </div>
-          </div>
-          <div className="w-full  text-lg">
-            <div className="flex-reverse flex flex-col flex-col-reverse items-center justify-between border-b py-4 lg:flex-row">
-              <div className="w-full lg:w-auto">
-                <p className="text-base lg:text-lg">
-                  <span className="pr-4 text-2xl text-primary lg:text-lg">
-                    日時
-                  </span>
-                  {seminar && getSeminarDateTime(seminar)}
+          )}
+          <div className="flex flex-col items-center bg-white px-7 lg:px-[60px]">
+            <div className="mt-4 flex w-full flex-col items-center">
+              <div className="flex w-full items-center justify-center rounded-md bg-[#e2e7ff] py-4">
+                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary lg:h-20 lg:w-20 m-2">
+                  <img
+                    src="/images/seminar/video.svg"
+                    className="w-[19px] lg:w-auto"
+                  />
+                </div>
+                <p className="lg:pl-4 font-bold text-primary lg:text-2xl flex-1">
+                  {seminar && seminar.subject}
                 </p>
               </div>
             </div>
-            <div className="border-b py-4">
-              <div>
-                <p className="text-base lg:text-lg">
-                  <span className="pr-4 text-2xl text-primary lg:text-lg lg:font-bold">
-                    講師
-                  </span>{' '}
-                  {seminar?.doctor_name !== undefined
-                    ? seminar.doctor_name
-                    : ''}
-                  先生
+            <div className="w-full  text-lg">
+              <div className="flex-reverse flex flex-col-reverse items-center justify-between border-b py-4 lg:flex-row">
+                <div className="w-full lg:w-auto">
+                  <p className="text-base lg:text-lg">
+                    <span className="pr-4 text-2xl text-primary lg:text-lg">
+                      日時
+                    </span>
+                    {seminar && getSeminarDateTime(seminar)}
+                  </p>
+                </div>
+              </div>
+              <div className="border-b py-4">
+                <div>
+                  <p className="text-base lg:text-lg">
+                    <span className="pr-4 text-2xl text-primary lg:text-lg lg:font-bold">
+                      講師
+                    </span>{' '}
+                    {seminar?.doctor_name !== undefined
+                      ? seminar.doctor_name
+                      : ''}
+                    先生
+                  </p>
+                </div>
+              </div>
+              <div className="pt-8">
+                <div>
+                  <p className="pr-4 text-2xl font-bold text-primary lg:text-2xl">
+                    セミナー概要
+                  </p>
+                  <p className="pt-4 pb-8 text-base">
+                    {seminar?.description !== undefined
+                      ? seminar.description
+                      : ''}
+                  </p>
+                </div>
+              </div>
+              <div className="rounded-lg border p-4 text-base">
+                <p className="pb-2">{seminar && seminar?.doctor_name}先生</p>
+                <p className="text-primary">
+                  {seminar && seminar?.doctor_profile}
                 </p>
               </div>
+              <a href={`/newChatRoom?target_account_id=${seminar?.account_id}`}>
+                <PrimaryButton className="mt-8 text-left">
+                  この先生にE-コンサルで相談をする
+                </PrimaryButton>
+              </a>
+              {seminar && seminar?.seminar_reviews.length > 0 && (
+                <div>
+                  <h2 className="pb-4 pt-10 text-2xl font-bold text-primary">
+                    セミナー参加者からの感想
+                  </h2>
+                  {seminar.seminar_reviews.map((review) => (
+                    <div
+                      className="mb-4 rounded-md bg-slate-100 px-4 py-6"
+                      key={review.id}
+                    >
+                      <p>{review.body}</p>
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
-            <div className="pt-8">
-              <div>
-                <p className="pr-4 text-2xl font-bold text-primary lg:text-2xl">
-                  セミナー概要
-                </p>
-                <p className="pt-4 pb-8 text-base">
-                  {seminar?.description !== undefined
-                    ? seminar.description
-                    : ''}
-                </p>
-              </div>
-            </div>
-            <div className="rounded-lg border p-4 text-base">
-              <p className="pb-2">{seminar && seminar?.doctor_name}先生</p>
-              <p className="text-primary">
-                {seminar && seminar?.doctor_profile}
-              </p>
-            </div>
-            <a href={`/newChatRoom?target_account_id=${seminar?.account_id}`}>
-              <PrimaryButton className="mt-8 text-left">
-                この先生にE-コンサルで相談をする
-              </PrimaryButton>
-            </a>
-            {seminar && seminar?.seminar_reviews.length > 0 && (
-              <div>
-                <h2 className="pb-4 pt-10 text-2xl font-bold text-primary">
-                  セミナー参加者からの感想
-                </h2>
-                {seminar.seminar_reviews.map((review) => (
-                  <div
-                    className="mb-4 rounded-md bg-slate-100 px-4 py-6"
-                    key={review.id}
-                  >
-                    <p>{review.body}</p>
-                  </div>
-                ))}
-              </div>
-            )}
           </div>
         </div>
       </div>
