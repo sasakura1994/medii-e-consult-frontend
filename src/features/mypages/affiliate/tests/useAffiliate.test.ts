32ac3a775d9c15bc38fda6a1858ffcff4151cb5f
sasakura1994 <kanazawa01211123@gmail.com>
2023-06-15 01:06:12 +0900

    テストを実装

--- src/features/mypages/affiliate/tests/useAffiliate.test.ts
+++ src/features/mypages/affiliate/tests/useAffiliate.test.ts
@@ -1,39 +1,54 @@
-// import 'cross-fetch/polyfill';
-// import { renderHook, act } from '@testing-library/react';
+import 'cross-fetch/polyfill';
+import { renderHook, waitFor, cleanup } from '@testing-library/react';
 import { useAffiliate } from '../useAffiliate';
-// import { UseAffiliateType } from '../useAffiliate';
+import { useToken } from '@/hooks/authentication/useToken';
 
-// const mockAccountId = 'example_account_id';
-// const mockQrCodeUrl = 'http://example.com/qr_code.png';
+const mockQrCodeUrl = 'http://example.com/qr_code.png';
+
+jest.mock('@/hooks/authentication/useToken', () => ({
+  useToken: jest.fn(),
+}));
 
 describe('useAffiliate', () => {
-  //   beforeEach(() => {
-  //     // createObjectURL のモック
-  //     global.URL.createObjectURL = jest.fn().mockReturnValue(mockQrCodeUrl);
-  //     // navigator のモック
-  //     Object.assign(navigator, {
-  //       clipboard: {
-  //         text: '',
-  //         readText() {
-  //           return Promise.resolve(this.text);
-  //         },
-  //         writeText(data: string) {
-  //           this.text = data;
-  //           return Promise.resolve();
-  //         },
-  //       },
-  //     });
-  //   });
-  //   afterEach(() => {
-  //     jest.restoreAllMocks();
-  //     Object.assign(navigator, { clipboard: undefined });
-  //   });
+  beforeEach(() => {
+    // createObjectURL のモック
+    global.URL.createObjectURL = jest.fn().mockReturnValue(mockQrCodeUrl);
+
+    // navigator のモック
+    Object.assign(navigator, {
+      clipboard: {
+        text: '',
+        readText() {
+          return Promise.resolve(this.text);
+        },
+        writeText(data: string) {
+          this.text = data;
+          return Promise.resolve();
+        },
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    Object.assign(navigator, { clipboard: undefined });
+    cleanup();
+  });
+
   test('should get qrcode url when to called fetchQrCode.', async () => {
-    //   let hookResult: { current: UseAffiliateType } | undefined;
-    //   act(() => {
-    //     hookResult = renderHook(() => useAffiliate()).result;
-    //   });
-    //   expect(hookResult?.current.isError).toBe(false);
-    //   expect(hookResult?.current.qrCodeUrl).toBe(mockQrCodeUrl);
+    (useToken as jest.Mock).mockReturnValue({ accountId: 'accountId' });
+    const { result } = renderHook(() => useAffiliate());
+    await waitFor(() => {
+      expect(result.current.isError).toBe(false);
+      expect(result.current.qrCodeUrl).toBe(mockQrCodeUrl);
+    });
+  });
+
+  test('アカウントIDを含むURLが取得できること', async () => {
+    (useToken as jest.Mock).mockReturnValue({ accountId: 'accountId' });
+    const { result } = renderHook(() => useAffiliate());
+    await waitFor(() => {
+      expect(result.current.invitationUrl).toContain('accountId');
+    });
   });
 });
