7157133b93db7eb2a11cee88fb810ebfd6227b4b
sasakura1994 <kanazawa01211123@gmail.com>
2023-06-20 09:06:22 +0900

    canvasの追加

--- src/features/chat/ChatTextInput.tsx
+++ src/features/chat/ChatTextInput.tsx
@@ -1,7 +1,50 @@
-import React, { useRef, useEffect } from 'react';
+import ImageEditor, {
+  ImageEditorProps,
+} from '@/components/Parts/ImageEditor/ImageEditor';
+import dynamic, { DynamicOptions } from 'next/dynamic';
+import React, {
+  useRef,
+  useEffect,
+  useState,
+  useCallback,
+  ChangeEvent,
+} from 'react';
+// canvasの関係でサーバー時点でimportできないため、下記のようにdynamic importする
+const ImageEditorComponent = dynamic<ImageEditorProps>(
+  (() =>
+    import(
+      '@/components/Parts/ImageEditor/ImageEditor'
+    )) as DynamicOptions<ImageEditorProps>,
+  { ssr: false }
+) as typeof ImageEditor;
 
 export const ChatTextInput = () => {
   const textInputRef = useRef<HTMLTextAreaElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const [editingImage, setEditingImage] = useState<File>();
+
+  const onSelectImage = useCallback(
+    async (e: ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      if (!e.target.files || e.target.files.length === 0) {
+        return;
+      }
+
+      const files = e.target.files;
+
+      if (files[0].type.match(/^image\//)) {
+        setEditingImage(files[0]);
+        return;
+      }
+    },
+    []
+  );
+
+  const resetImageInput = useCallback(() => {
+    if (imageInputRef.current) {
+      imageInputRef.current.value = '';
+    }
+  }, [imageInputRef]);
 
   const resizeHeight = () => {
     if (textInputRef.current) {
@@ -28,16 +71,34 @@ export const ChatTextInput = () => {
           placeholder-gray-600 disabled:bg-[#d5d5d5] disabled:text-block-gray"
           placeholder="メッセージを入力 (Shift + Enterキーで送信)"
         />
+        <input
+          type="file"
+          name="file"
+          ref={imageInputRef}
+          className="hidden"
+          onChange={onSelectImage}
+          onClick={() => resetImageInput()}
+        />
         <img
           src="/icons/clip_message.svg"
           alt=""
-          className="my-auto ml-3 h-[30px] w-[30px]"
+          className="my-auto ml-3 h-[30px] w-[30px] cursor-pointer"
+          onClick={() => imageInputRef.current?.click()}
         />
         <img
           src="/icons/send_message.svg"
           alt=""
-          className="my-auto ml-3 h-[30px] w-[30px]"
+          className="my-auto ml-3 h-[30px] w-[30px] cursor-pointer"
         />
+        {editingImage && (
+          <ImageEditorComponent
+            file={editingImage}
+            onSubmit={() => {
+              console.log('submit');
+            }}
+            onClose={() => setEditingImage(undefined)}
+          />
+        )}
       </div>
     </div>
   );
