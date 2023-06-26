be2ec2d3d152f49e71098f0b1d26b805e88850df
Shoichi Namba <alphabrend@gmail.com>
2023-06-26 15:06:28 +0900

    useEditProfilePageのテスト追加

--- src/features/mypages/editProfile/useEditProfilePage.ts
+++ src/features/mypages/editProfile/useEditProfilePage.ts
@@ -7,7 +7,13 @@ type Query = {
 
 type EditProfileMode = 'profile' | 'edit';
 
-export const useEditProfilePage = () => {
+type UseEditProfilePage = {
+  editProfileMode: EditProfileMode;
+  isRegisterMode: boolean;
+  setSelectedEditProfileMode: Dispatch<SetStateAction<EditProfileMode>>;
+};
+
+export const useEditProfilePage = (): UseEditProfilePage => {
   const router = useRouter();
   const query = router.query as Query;
   const [selectedEditProfileMode, setSelectedEditProfileMode] = useState<EditProfileMode>('profile');
