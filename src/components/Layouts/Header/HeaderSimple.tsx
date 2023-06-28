adc6b0239fd86c3d7de420cd9e896476c0d4f052
Shoichi Namba <alphabrend@gmail.com>
2023-06-28 15:36:15 +0900

    ログイン前ヘッダー調整

--- src/components/Layouts/Header/HeaderSimple.tsx
+++ src/components/Layouts/Header/HeaderSimple.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { HeaderContainer } from './HeaderContainer';
 import { HeaderLogo } from './HeaderLogo';
-import { HeaderMenuContainer } from './HeaderMenuContainer';
 import { HeaderMenuList } from './HeaderMenuList';
 import { HeaderMenuListItem } from './HeaderMenuListItem';
 
 export const HeaderSimple: React.FC = () => {
   return (
     <HeaderContainer>
-      <HeaderLogo />
-
-      <HeaderMenuContainer>
-        {/* PC Menu */}
+      <div className="flex items-center">
+        <HeaderLogo />
         <HeaderMenuList>
           <HeaderMenuListItem href="/login">ログイン</HeaderMenuListItem>
         </HeaderMenuList>
-      </HeaderMenuContainer>
+      </div>
     </HeaderContainer>
   );
 };
