bd25243fc50c08707779a8489075f27c2f071abc
sasakura1994 <kanazawa01211123@gmail.com>
2023-07-12 14:35:04 +0900

    tutorial=trueの場合の処理追加

--- src/features/top/useTop.ts
+++ src/features/top/useTop.ts
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 type UseTop = {
@@ -6,8 +7,10 @@ type UseTop = {
 };
 
 export const useTop = (): UseTop => {
+  const router = useRouter();
+  const query = router.query;
   const [showTutorialExplanationModal, setShowTutorialExplanationModal] =
-    useState(false);
+    useState(query.tutorial === 'true');
   return {
     showTutorialExplanationModal,
     setShowTutorialExplanationModal,
