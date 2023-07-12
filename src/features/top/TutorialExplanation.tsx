750e3d1930f968c31d49e4fbea6425d1f5e853f0
sasakura1994 <kanazawa01211123@gmail.com>
2023-07-12 16:46:42 +0900

    指摘事項対応: スクロールが不要なのでなくす

--- src/features/top/TutorialExplanation.tsx
+++ src/features/top/TutorialExplanation.tsx
@@ -12,7 +12,7 @@ const TutorialExplanation = (props: TutorialExplanationProps) => {
 
   return (
     <Modal
-      className="relative flex w-full flex-col lg:h-4/5 lg:w-3/5"
+      className="relative flex w-full flex-col lg:w-1/2"
       setShowModal={setShowModal}
       isCenter
     >
@@ -25,37 +25,15 @@ const TutorialExplanation = (props: TutorialExplanationProps) => {
         <img src="icons/close_primary.svg" alt="" />
       </div>
       <div className="relative flex h-full flex-col rounded-lg bg-white px-6 py-2 text-center">
-        <div className="mx-auto mb-14 h-full w-full overflow-y-auto">
-          {page === 1 ? (
-            <div className="w-full">
-              <img
-                src="images/top/tutorial1.png"
-                alt="tutorial1"
-                className="h-auto w-full"
-              />
-            </div>
-          ) : page === 2 ? (
-            <div className="w-full">
-              <img
-                src="images/top/tutorial2.png"
-                className="h-auto w-full"
-                alt="tutorial2"
-              />
-            </div>
-          ) : (
-            <div className="w-full">
-              <img
-                src="images/top/tutorial3.png"
-                className="h-auto w-full"
-                alt="tutorial3"
-              />
-            </div>
-          )}
-        </div>
-        <div
-          className="absolute bottom-0 right-0 left-0 mx-auto mb-4
-        flex items-center justify-center space-x-4 text-2xl text-gray-400"
-        >
+        {page === 1 ? (
+          <img src="images/top/tutorial1.png" alt="tutorial1" />
+        ) : page === 2 ? (
+          <img src="images/top/tutorial2.png" alt="tutorial2" />
+        ) : (
+          <img src="images/top/tutorial3.png" alt="tutorial3" />
+        )}
+
+        <div className="mx-auto mb-4 flex items-center justify-center space-x-4 text-2xl text-gray-400">
           {page !== 1 ? (
             <img
               onClick={back}
