981aec87cd79bc502634df716236add097439119
Shoichi Namba <alphabrend@gmail.com>
2023-07-13 19:25:25 +0900

    chatRoomの初期化テスト追加

--- src/features/chat/newChatRoom/tests/useNewChatRoom.test.ts
+++ src/features/chat/newChatRoom/tests/useNewChatRoom.test.ts
@@ -1,10 +1,52 @@
 import 'cross-fetch/polyfill';
-import { renderHook, act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import { useNewChatRoom } from '../useNewChatRoom';
-import { DoctorEntity } from '@/types/entities/doctorEntity';
-import { GroupEntity } from '@/types/entities/GroupEntity';
 import { RecoilRoot } from 'recoil';
+import { useRouter } from 'next/router';
+
+jest.mock('next/router');
 
 describe('useNewChatROom', () => {
-  test.todo('あとで書く');
+  describe('chatRoom', () => {
+    test('room_type:専門医指定方法のクエリが存在', () => {
+      const useRouterMock = useRouter as jest.Mocked<typeof useRouter>;
+      (useRouterMock as jest.Mock).mockReturnValue({
+        query: { room_type: 'BY_NAME' },
+      });
+
+      const { result } = renderHook(() => useNewChatRoom(), {
+        wrapper: RecoilRoot,
+      });
+
+      expect(result.current.chatRoom.room_type).toBe('BY_NAME');
+    });
+
+    test('target_account_id:医師を指定', () => {
+      const useRouterMock = useRouter as jest.Mocked<typeof useRouter>;
+      (useRouterMock as jest.Mock).mockReturnValue({
+        query: { target_account_id: 'accountid' },
+      });
+
+      const { result } = renderHook(() => useNewChatRoom(), {
+        wrapper: RecoilRoot,
+      });
+
+      expect(result.current.chatRoom.target_doctor).toBe('accountid');
+      expect(result.current.chatRoom.room_type).toBe('BY_NAME');
+    });
+
+    test('target_group_id:グループを指定', () => {
+      const useRouterMock = useRouter as jest.Mocked<typeof useRouter>;
+      (useRouterMock as jest.Mock).mockReturnValue({
+        query: { target_group_id: 'group1' },
+      });
+
+      const { result } = renderHook(() => useNewChatRoom(), {
+        wrapper: RecoilRoot,
+      });
+
+      expect(result.current.chatRoom.group_id).toBe('group1');
+      expect(result.current.chatRoom.room_type).toBe('GROUP');
+    });
+  });
 });
