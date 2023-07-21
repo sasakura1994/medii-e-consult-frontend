f438c96cbf48d658a367e0d1fd7f5da30ff13d58
watanabe-medii-jp <t.watanabe@medii.jp>
2023-07-21 09:24:43 +0900

    urlに特定の文字列が含まれるかのように基準を逆にした

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -94,7 +94,7 @@ const App = (props: AppPropsWithLayout) => {
         '/seminar',
         '/seminar/archives',
         '/top',
-      ].includes(url.toLowerCase())
+      ].some((str) => url.toLocaleLowerCase().includes(str))
     ) {
       window.location.href = url.toLowerCase();
       throw 'routeChange aborted.';
@@ -116,6 +116,6 @@ const App = (props: AppPropsWithLayout) => {
 };
 
 // 初回queryのundefined現象を解決する
-App.getInitialProps = async () => ( { pageProps: {} } );
+App.getInitialProps = async () => ({ pageProps: {} });
 
 export default App;
