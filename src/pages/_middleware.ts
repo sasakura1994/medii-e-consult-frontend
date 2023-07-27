47d00aca0ef28746ad83aa4fd08b0510564cd9f2
Hashimoto <hash419@gmail.com>
2023-07-27 20:46:30 +0900

    middleware修正

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -4,6 +4,12 @@ const Middleware = (req: NextRequest) => {
   if (req.nextUrl.pathname.toLowerCase() !== req.nextUrl.pathname) {
     const url = req.nextUrl.clone();
     url.pathname = req.nextUrl.pathname.toLowerCase();
+
+    // 擬似ヘッダーを取得する処理
+    const authority = req.headers.get('authority');
+    // 取得した擬似ヘッダーをコンソールに出力
+    console.log('送信元:', authority);
+
     return NextResponse.redirect(url);
   }
   return NextResponse.next();
