63a078c5a67c611bcd4d9ef20f62d14cb6f319cb
Hashimoto <hash419@gmail.com>
2023-07-31 10:29:36 +0900

    大文字小文字の変換はrewriteに変更

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -5,13 +5,7 @@ const Middleware = (req: NextRequest) => {
     const url = req.nextUrl.clone();
     url.pathname = req.nextUrl.pathname.toLowerCase();
 
-    // Authorityヘッダーが外部か否かでサブディレクトリ化
-    const authority = req.headers.get('authority');
-    if (authority != null && !authority.endsWith("medii.jp")) {
-      url.pathname = process.env.EX_WEB_DIR + url.pathname;
-    }
-
-    return NextResponse.redirect(url);
+    return NextResponse.rewrite(url);
   }
   return NextResponse.next();
 };
