20006eeae586b1b98e3394dc67815a20ba0074e5
Shoichi Namba <alphabrend@gmail.com>
2023-08-02 15:43:53 +0900

    確認画面の下書き画像プレビュー追加

--- src/features/chat/newChatRoom/NewChatRoomConfirmation.tsx
+++ src/features/chat/newChatRoom/NewChatRoomConfirmation.tsx
@@ -9,14 +9,11 @@ import { GrayButton } from '@/components/Parts/Button/GrayButton';
 type Props = ReturnType<typeof useNewChatRoom>;
 
 export const NewChatRoomConfirmation: React.FC<Props> = (props: Props) => {
-  const { ageRange, backToInput, childAge, chatRoom, isSending, submit } =
-    props;
+  const { ageRange, backToInput, chatDraftImages, childAge, chatRoom, isSending, submit } = props;
 
   return (
     <>
-      <h1 className="text-center text-2xl leading-9">
-        E-コンサル ルーム プレビュー
-      </h1>
+      <h1 className="text-center text-2xl leading-9">E-コンサル ルーム プレビュー</h1>
       <div className="mx-auto mb-10 lg:w-[80%]">
         <div className="my-8 text-center text-sm text-strong">
           &lt;注意&gt;本サービスを通して得た専門的知見に基づくアドバイスは、
@@ -27,36 +24,33 @@ export const NewChatRoomConfirmation: React.FC<Props> = (props: Props) => {
           <div>{ageRange === 'child' ? `${childAge}歳` : `${ageRange}代`}</div>
           <div>{chatRoom.gender === 'man' ? '男性' : '女性'}</div>
         </NewChatRoomConfirmationValue>
-        <NewChatRoomConfirmationLabel className="mt-10">
-          要約
-        </NewChatRoomConfirmationLabel>
-        <NewChatRoomConfirmationValue className="mt-4">
-          {chatRoom.disease_name}
-        </NewChatRoomConfirmationValue>
-        <NewChatRoomConfirmationLabel className="mt-10">
-          コンサル文
-        </NewChatRoomConfirmationLabel>
-        <div className="mt-4 whitespace-pre-wrap break-words text-sm">
-          {chatRoom.first_message}
-        </div>
+        <NewChatRoomConfirmationLabel className="mt-10">要約</NewChatRoomConfirmationLabel>
+        <NewChatRoomConfirmationValue className="mt-4">{chatRoom.disease_name}</NewChatRoomConfirmationValue>
+        <NewChatRoomConfirmationLabel className="mt-10">コンサル文</NewChatRoomConfirmationLabel>
+        <div className="mt-4 whitespace-pre-wrap break-words text-sm">{chatRoom.first_message}</div>
+        {chatDraftImages && (
+          <div className="mt-8 flex flex-col gap-4">
+            {chatDraftImages.map((chatDraftImage) => (
+              <div key={chatDraftImage.chat_draft_image_id}>
+                {chatDraftImage.is_image ? (
+                  <img src={chatDraftImage.url} alt={chatDraftImage.file_name} />
+                ) : (
+                  <>{chatDraftImage.file_name}</>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
         <div className="mt-8 text-center">
           {!isSending ? (
             <div className="mx-auto w-3/5">
               <div>
-                <GrayButton
-                  type="button"
-                  onClick={() => backToInput()}
-                  className="w-full"
-                >
+                <GrayButton type="button" onClick={() => backToInput()} className="w-full">
                   編集
                 </GrayButton>
               </div>
               <div className="mt-4">
-                <PrimaryButton
-                  type="button"
-                  onClick={() => submit()}
-                  className="w-full"
-                >
+                <PrimaryButton type="button" onClick={() => submit()} className="w-full">
                   E-コンサルを開始
                 </PrimaryButton>
               </div>
