05dc6d79bcfa388f7912b8ff319f2295e978de98
Shoichi Namba <alphabrend@gmail.com>
2023-08-04 18:12:27 +0900

    完了ページのデザイン調整

--- src/pages/withdrawal/completed.tsx
+++ src/pages/withdrawal/completed.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import type { NextPageWithLayout } from '@/pages/_app';
-import { Card } from '@/components/Parts/Card/Card';
 import { HeaderLogoOnly } from '@/components/Layouts/Header/HeaderLogoOnly';
 
 const WithdrawCompletedPage: NextPageWithLayout = () => {
   return (
-    <div className="flex h-[100vh] flex-col bg-bg">
+    <div className="bg-bg-primary flex h-[100vh] flex-col">
       <HeaderLogoOnly />
-      <div className="flex flex-1 items-center justify-center">
-        <Card className="mx-auto w-full px-8 py-20 lg:w-[500px] lg:px-20">
-          <div className="text-center">
-            <p>
-              アカウント削除が完了しました。
-              <br />
-              またのご利用をお待ちしております。
-            </p>
-          </div>
-        </Card>
+      <div className="mx-auto mt-8 max-w-[1024px] px-4 pb-20 text-center lg:px-0">
+        <h2 className="text-xxl font-semibold">アカウント削除が完了しました</h2>
+        <p className="mt-2 text-md">
+          ご利用ありがとうございました。
+          <br />
+          またのご利用を心よりお待ちしております。
+        </p>
       </div>
     </div>
   );
