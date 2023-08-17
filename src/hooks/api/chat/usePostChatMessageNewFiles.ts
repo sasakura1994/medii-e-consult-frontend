29d43da6d7f7f43858f404d74fd580c8f2f2b0d6
sasakura1994 <kanazawa01211123@gmail.com>
2023-08-17 15:25:16 +0900

    content-typeの変更

--- src/hooks/api/chat/usePostChatMessageNewFiles.ts
+++ src/hooks/api/chat/usePostChatMessageNewFiles.ts
@@ -11,18 +11,14 @@ export type PostChatMessageNewFilesResponseData = {
 };
 
 export const usePostChatMessageNewFiles = () => {
-  const { axios } = useAxios();
+  const { axios } = useAxios('multipart/form-data');
 
   const postNewFile = useCallback(
     (data: PostChatMessageNewFilesRequestData) => {
       const formData = new FormData();
       formData.append('uploaded_file', data.uploaded_file);
       formData.append('chat_room_id', data.chat_room_id);
-      return axios.post<PostChatMessageNewFilesResponseData>('/chat_message/new_files', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
+      return axios.post<PostChatMessageNewFilesResponseData>('/chat_message/new_files', formData);
     },
     [axios]
   );
