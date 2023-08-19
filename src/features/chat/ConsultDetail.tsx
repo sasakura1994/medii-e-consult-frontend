52595882754aef71524c46cb8c5c84658fd7273e
sasakura1994 <kanazawa01211123@gmail.com>
2023-08-19 14:34:56 +0900

    ラベルの出しわけ

--- src/features/chat/ConsultDetail.tsx
+++ src/features/chat/ConsultDetail.tsx
@@ -83,6 +83,18 @@ export const ConsultDetail = (props: ConsultDetailProps) => {
     }
   }, [chatListData, chatRoomData, getExperienceYear, getSpecialityName]);
 
+  const isCloseRoom = useMemo(() => {
+    if (chatRoomData) {
+      return chatRoomData.chat_room.status === 'RESOLVED' || chatRoomData.chat_room.status === 'CLOSED';
+    }
+  }, [chatRoomData]);
+
+  const isChatRoomOwner = useMemo(() => {
+    if (chatRoomData) {
+      return chatRoomData.chat_room.owner_account_id === accountId;
+    }
+  }, [chatRoomData, accountId]);
+
   // チャットリストが更新される度にスクロールを一番下にする
   useEffect(() => {
     if (chatListRef.current) {
@@ -96,19 +108,35 @@ export const ConsultDetail = (props: ConsultDetailProps) => {
         <div className="flex h-[calc(100vh-62px)] w-[787px] flex-col border border-[#d5d5d5]">
           <div className="flex-shrink-0 flex-grow-0">
             <div className="mr-2 flex h-14 items-center space-x-1">
-              <div className="ml-4 flex w-[53px] items-center justify-center rounded-full bg-strong">
-                <p className="py-0.5 text-xs text-white">未解決</p>
-              </div>
+              {isCloseRoom ? (
+                <div className="ml-4 flex w-[53px] items-center justify-center rounded-full bg-[#64abc4]">
+                  <p className="py-0.5 text-xs text-white">解決済</p>
+                </div>
+              ) : (
+                <div className="ml-4 flex w-[53px] items-center justify-center rounded-full bg-strong">
+                  <p className="py-0.5 text-xs text-white">未解決</p>
+                </div>
+              )}
               <p className="ml-2 flex-grow font-bold">{chatRoomData.chat_room.title}</p>
-              <button className="h-9 w-[78px] rounded-full bg-primary">
-                <p className="text-xs text-white">返答依頼</p>
-              </button>
-              <button className="h-9 w-[126px] rounded-full bg-primary">
-                <p className="text-xs text-white">コンサル終了依頼</p>
-              </button>
-              <button className="h-9 w-[78px] rounded-full bg-strong">
-                <p className="text-xs text-white">回答パス</p>
-              </button>
+              {!isCloseRoom ? (
+                <>
+                  <button className="h-9 w-[78px] rounded-full bg-primary">
+                    <p className="text-xs text-white">返答依頼</p>
+                  </button>
+                  <button className="h-9 w-[126px] rounded-full bg-primary">
+                    <p className="text-xs text-white">コンサル終了依頼</p>
+                  </button>
+                  <button className="h-9 w-[78px] rounded-full bg-strong">
+                    <p className="text-xs text-white">回答パス</p>
+                  </button>
+                </>
+              ) : isChatRoomOwner && chatRoomData.chat_room.room_type !== 'GROUP' ? (
+                <button className="h-9 w-[138px] rounded-full bg-primary">
+                  <p className="text-xs text-white">他の医師に相談する</p>
+                </button>
+              ) : (
+                <></>
+              )}
               <img src="/icons/btn_menu.svg" alt="" className="h-9 w-9 cursor-pointer" />
             </div>
             <div className="flex h-5 items-center space-x-1 border">
@@ -135,10 +163,12 @@ export const ConsultDetail = (props: ConsultDetailProps) => {
             </div>
           </div>
           <div className="relative flex overflow-hidden">
-            <div className="-mb-3 flex-1 overflow-scroll" ref={chatListRef}>
+            <div className="-mb-3 flex-1 overflow-scroll bg-bg" ref={chatListRef}>
               <ChatList chatListData={chatListDataWithDisplayName} currentUserAccountId={accountId} />
             </div>
-            <div className="pointer-events-none absolute inset-0 overflow-hidden bg-black bg-opacity-20" />
+            {isCloseRoom && (
+              <div className="pointer-events-none absolute inset-0 overflow-hidden bg-black bg-opacity-20" />
+            )}
           </div>
           <div className="flex-shrink-0 flex-grow-0">
             <ChatTextInput chatRoomId={chatRoomData.chat_room.chat_room_id} />
