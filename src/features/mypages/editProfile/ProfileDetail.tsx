d9e7acc6ca2439833a0a54d74edaf996a5de0eed
dala00 <alphabrend@gmail.com>
2023-08-21 10:29:24 +0900

    文言修正
    Co-authored-by: sako <57947664+sako0@users.noreply.github.com>

--- src/features/mypages/editProfile/ProfileDetail.tsx
+++ src/features/mypages/editProfile/ProfileDetail.tsx
@@ -142,7 +142,7 @@ export const ProfileDetail: React.FC<Props> = ({ onEdit }: Props) => {
 
             <div className="mt-12 lg:pb-10">
               <Link href="/withdrawal/confirm">
-                <a className="text-md text-distructive underline">アカウントを削除する</a>
+                <a className="text-md text-distructive underline">アカウントを削除</a>
               </Link>
             </div>
           </>
