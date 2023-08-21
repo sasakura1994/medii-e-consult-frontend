f33e9bf68dd79b51942988c6f8e7bacc5ae7b49d
sasakura1994 <kanazawa01211123@gmail.com>
2023-08-21 11:04:14 +0900

    不要なstyled-componentsのコンポーネントを削除

--- src/features/chat/styled.tsx
+++ src/features/chat/styled.tsx
@@ -1,14 +1,5 @@
 import styled, { keyframes } from 'styled-components';
 
-// TODO: こちら不要なので削除
-export const StyledHiddenScrollBar = styled.div`
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
 const fulfillingSquareSpinnerAnimation = keyframes`
   0% {
     transform: rotate(0deg);
