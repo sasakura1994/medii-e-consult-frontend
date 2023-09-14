4cbe64b12bee29cde9291686232f5d9d2f0d30d7
Shoichi Namba <alphabrend@gmail.com>
2023-09-14 14:42:46 +0900

    数が多い時は３列

--- src/features/onboarding/QuestionaryItems.tsx
+++ src/features/onboarding/QuestionaryItems.tsx
@@ -12,5 +12,9 @@ export const QuestionaryItems = (props: Props) => {
     return <div className="mt-2 flex gap-4 text-md">{children}</div>;
   }
 
+  if (itemCount >= 10) {
+    return <div className="mt-2 flex flex-col items-start gap-1 text-md lg:grid lg:grid-cols-3">{children}</div>;
+  }
+
   return <div className="mt-2 flex flex-col items-start gap-1 text-md">{children}</div>;
 };
