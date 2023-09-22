fb03733fd47c215b39071fd6ff2ebcdb32a7cd07
Shoichi Namba <alphabrend@gmail.com>
2023-09-22 17:35:16 +0900

    テスト整備

--- __tests__/hooks/useRegister.test.ts
+++ __tests__/hooks/useRegister.test.ts
@@ -1,5 +1,5 @@
-import { usePostLogin } from '@/hooks/api/doctor/usePostLogin';
-import { useLogin } from '@/hooks/useLogin';
+import { loginRedirectUrlKey } from '@/data/localStorage';
+import { useAxios } from '@/hooks/network/useAxios';
 import { useRegister } from '@/hooks/useRegister';
 import { act, renderHook } from '@testing-library/react';
 import { useRouter } from 'next/router';
@@ -7,6 +7,7 @@ import { RecoilRoot } from 'recoil';
 
 jest.mock('next/router');
 jest.mock('@/hooks/api/doctor/usePostLogin');
+jest.mock('@/hooks/network/useAxios');
 
 describe('useRegister', () => {
   describe('queryString', () => {
@@ -17,10 +18,12 @@ describe('useRegister', () => {
         },
         writable: true,
       });
+      (useAxios as jest.Mock).mockReturnValue({
+        axios: {},
+      });
 
       const pushMock = jest.fn();
-      const useRouterMock = useRouter as jest.Mocked<typeof useRouter>;
-      (useRouterMock as jest.Mock).mockReturnValue({
+      (useRouter as jest.Mock).mockReturnValue({
         query: {},
         push: pushMock,
       });
@@ -31,5 +34,66 @@ describe('useRegister', () => {
 
       expect(hooks.current.queryString).toBe('?redirect=/seminar');
     });
+  });
+
+  test('register', async () => {
+    const setItem = jest.spyOn(Storage.prototype, 'setItem');
+
+    (useRouter as jest.Mock).mockReturnValue({
+      query: { redirect: '/seminar' },
+    });
+
+    const post = jest.fn();
+    post.mockResolvedValue({
+      data: true,
+    });
+    (useAxios as jest.Mock).mockReturnValue({
+      axios: {
+        post,
+      },
+    });
+
+    const hooks = renderHook(() => useRegister(), {
+      wrapper: RecoilRoot,
+    }).result;
+
+    act(() => {
+      hooks.current.setEmail('test@example.com');
+      hooks.current.setIsPrivacyPolicyAgree(true);
+      hooks.current.setIsTermsAgree(true);
+    });
+
+    await act(() => hooks.current.register());
+
+    expect(hooks.current.isSent).toBeTruthy();
+    expect(setItem).toBeCalledWith(loginRedirectUrlKey, '/seminar');
+  });
+
+  test('goToSnsLogin', async () => {
+    const setItem = jest.spyOn(Storage.prototype, 'setItem');
+
+    const pushMock = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({
+      query: { redirect: '/seminar', p: 'accountid' },
+      push: pushMock,
+    });
+
+    (useAxios as jest.Mock).mockReturnValue({
+      axios: {},
+    });
+
+    const hooks = renderHook(() => useRegister(), {
+      wrapper: RecoilRoot,
+    }).result;
+
+    act(() => {
+      hooks.current.setIsPrivacyPolicyAgree(true);
+      hooks.current.setIsTermsAgree(true);
+    });
+
+    await act(() => hooks.current.goToSnsLogin());
+
+    expect(setItem).toBeCalledWith(loginRedirectUrlKey, '/seminar');
+    expect(pushMock).toBeCalledWith('/snsregistration?p=accountid');
   });
 });
