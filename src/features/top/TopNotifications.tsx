5bf24c340fe51890b76ccc355b20a1ca6b9af229
Shoichi Namba <alphabrend@gmail.com>
2023-09-26 11:50:10 +0900

    トップページの通知欄調整

--- src/features/top/TopNotifications.tsx
+++ src/features/top/TopNotifications.tsx
@@ -2,12 +2,49 @@ import { InlineNotification } from '@/components/Notification/InlineNotification
 import { useNmo } from '@/hooks/alliance/useNmo';
 import { useFetchProfile } from '@/hooks/api/doctor/useFetchProfile';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 export const TopNotifications = () => {
   const router = useRouter();
   const { profile } = useFetchProfile();
   const { isNeedToInputProfile } = useNmo();
+
+  const profileNotification = useMemo(() => {
+    if (!profile) {
+      return <></>;
+    }
+
+    if (profile.status.match(/^PENDING_/)) {
+      return (
+        <InlineNotification
+          text="現在、ご提出いただいた医師確認資料を確認中です。"
+          dataTestId="top-notification-pending"
+        />
+      );
+    }
+
+    if (profile.is_imperfect_profile) {
+      return (
+        <InlineNotification
+          text="プロフィール情報が入力されておりません。サービスをご利用いただくためにプロフィールのご入力をお願いいたします。"
+          dataTestId="top-notification-is-imperfect-profile"
+          buttonText="プロフィール画面を開く"
+          buttonOnClick={() => router.push('/editprofile?registerMode=1')}
+        />
+      );
+    }
+
+    if (profile.status === 'PROFILE' || (profile.status === 'CREATED' && profile.need_to_send_confimation)) {
+      return (
+        <InlineNotification
+          text="医師確認資料が提出されておりません。サービスをご利用いただくために医師確認資料のご提出をお願いいたします。"
+          dataTestId="top-notification-need-to-send-confirmation"
+          buttonText="プロフィール画面を開く"
+          buttonOnClick={() => router.push('/document')}
+        />
+      );
+    }
+  }, [profile, router]);
 
   if (!profile) {
     return <></>;
@@ -17,29 +54,12 @@ export const TopNotifications = () => {
     return (
       <InlineNotification
         text="すべてのサービスをご利用いただくには、追加のプロフィール入力が必要です。"
+        dataTestId="top-notification-nmo-input-profile"
         buttonText="プロフィール入力"
         buttonOnClick={() => router.push('/nmo/input-profile')}
       />
     );
   }
 
-  if (profile.is_imperfect_profile) {
-    return (
-      <InlineNotification
-        text="プロフィール情報が入力されておりません。お手数ですがサービスをご利用頂くためにプロフィール画面のご入力をお願いいたします。"
-        buttonText="プロフィール画面を開く"
-        buttonOnClick={() => router.push('/editprofile?registerMode=1')}
-      />
-    );
-  } else if (profile.status === 'PROFILE' || (profile.status === 'CREATED' && profile.need_to_send_confimation)) {
-    return (
-      <InlineNotification
-        text="確認資料が提出されておりません。お手数ですがサービスをご利用頂くためにプロフィール画面から確認資料をご提出ください。"
-        buttonText="プロフィール画面を開く"
-        buttonOnClick={() => router.push('/document')}
-      />
-    );
-  }
-
-  return <></>;
+  return <>{profileNotification}</>;
 };
---
+++ src/features/top/test/TopNotifications.test.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { RecoilRoot } from 'recoil';
+import { useFetchProfile } from '@/hooks/api/doctor/useFetchProfile';
+import { TopNotifications } from '../TopNotifications';
+
+jest.mock('@/hooks/api/doctor/useFetchProfile');
+
+describe('TopNotifications', () => {
+  describe('プロフィールの状況', () => {
+    test('プロフィール未入力', async () => {
+      (useFetchProfile as jest.Mock).mockReturnValue({
+        profile: {
+          status: 'CREATED',
+          is_imperfect_profile: true,
+        },
+      });
+
+      await act(async () => {
+        await render(
+          <RecoilRoot>
+            <TopNotifications />
+          </RecoilRoot>
+        );
+      });
+
+      expect(screen.queryByTestId('top-notification-is-imperfect-profile')).toBeInTheDocument();
+    });
+
+    test('医師確認未送信', async () => {
+      (useFetchProfile as jest.Mock).mockReturnValue({
+        profile: {
+          status: 'PROFILE',
+          is_imperfect_profile: false,
+          need_to_send_confimation: true,
+        },
+      });
+
+      await act(async () => {
+        await render(
+          <RecoilRoot>
+            <TopNotifications />
+          </RecoilRoot>
+        );
+      });
+
+      expect(screen.queryByTestId('top-notification-need-to-send-confirmation')).toBeInTheDocument();
+    });
+
+    test('医師確認承認待ち', async () => {
+      (useFetchProfile as jest.Mock).mockReturnValue({
+        profile: {
+          status: 'PENDING_AUTO',
+          is_imperfect_profile: false,
+          need_to_send_confimation: false,
+        },
+      });
+
+      await act(async () => {
+        await render(
+          <RecoilRoot>
+            <TopNotifications />
+          </RecoilRoot>
+        );
+      });
+
+      expect(screen.queryByTestId('top-notification-pending')).toBeInTheDocument();
+    });
+  });
+
+  test('nmoのためプロフィールが不足している場合', async () => {
+    (useFetchProfile as jest.Mock).mockReturnValue({
+      profile: {
+        registration_source: 'nmo',
+        last_name_hira: '',
+        status: 'CREATED',
+      },
+    });
+
+    await act(async () => {
+      await render(
+        <RecoilRoot>
+          <TopNotifications />
+        </RecoilRoot>
+      );
+    });
+
+    expect(screen.queryByTestId('top-notification-nmo-input-profile')).toBeInTheDocument();
+  });
+});
