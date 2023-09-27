2b59b7b6b9698d0c580f9cba27bc82cf6346c1df
Shoichi Namba <alphabrend@gmail.com>
2023-09-27 14:33:48 +0900

    上部の表示崩れ修正

--- src/pages/seminar/archives.tsx
+++ src/pages/seminar/archives.tsx
@@ -13,21 +13,27 @@ const Archives: NextPage = () => {
   const { page } = router.query;
   const current = page === undefined ? 1 : Number(page);
   const numberPerPage = 8;
-  const { seminars, ticketCount, maxPage, allItemsCount } =
-    useSeminars(current);
+  const { seminars, ticketCount, maxPage, allItemsCount } = useSeminars(current);
   const startNumber = (current - 1) * numberPerPage;
   const endNumber = current * numberPerPage - 1;
   return (
     <>
-      <div className="-mt-10 bg-[url('/images/seminar/SP_back.png')] bg-cover bg-no-repeat pb-12 pt-10 lg:bg-[url('/images/seminar/PC_back.png')]">
-        <div className="mx-auto flex w-full flex-col items-center rounded-lg px-6 pb-20 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] lg:max-w-[960px] lg:bg-white lg:px-20 lg:pt-14">
+      <div
+        className="
+          bg-[url('/images/seminar/SP_back.png')]
+          bg-cover bg-no-repeat pb-12 pt-10 lg:bg-[url('/images/seminar/PC_back.png')]
+        "
+      >
+        <div
+          className="
+            mx-auto flex w-full flex-col items-center rounded-lg px-6 pb-20 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]
+            lg:max-w-[960px] lg:bg-white lg:px-20 lg:pt-14
+          "
+        >
           <SeminarArchiveHeader ticketCount={ticketCount} />
           <p className="w-full pb-4">
             <span className="font-bold">
-              {startNumber + 1} ~{' '}
-              {allItemsCount && allItemsCount > endNumber + 1
-                ? endNumber + 1
-                : allItemsCount}
+              {startNumber + 1} ~ {allItemsCount && allItemsCount > endNumber + 1 ? endNumber + 1 : allItemsCount}
             </span>
             件を表示 / 全<span className="font-bold">{allItemsCount}</span>件
           </p>
@@ -46,23 +52,13 @@ const Archives: NextPage = () => {
                     className="inline-flex items-center rounded-full border
                border-primary px-10 py-3 text-center text-sm text-primary"
                   >
-                    <img
-                      src="/icons/arrow_left.svg"
-                      className="mr-2 inline h-3"
-                      alt=""
-                    />
+                    <img src="/icons/arrow_left.svg" className="mr-2 inline h-3" alt="" />
                     TOPへ戻る
                   </div>
                 </a>
               </Link>
             </div>
-            {maxPage && (
-              <Pagination
-                page={current}
-                maxPage={maxPage}
-                url="/seminar/archives"
-              />
-            )}
+            {maxPage && <Pagination page={current} maxPage={maxPage} url="/seminar/archives" />}
             <div className="hidden w-48 lg:block"></div>
           </div>
           <div className="block lg:hidden lg:w-48">
@@ -72,11 +68,7 @@ const Archives: NextPage = () => {
                   className="inline-flex items-center rounded-full border border-primary
              bg-white px-14 py-4 text-center text-sm text-primary lg:px-10 lg:py-3"
                 >
-                  <img
-                    src="/icons/arrow_left.svg"
-                    className="mr-2 inline h-3"
-                    alt=""
-                  />
+                  <img src="/icons/arrow_left.svg" className="mr-2 inline h-3" alt="" />
                   TOPへ戻る
                 </div>
               </a>
