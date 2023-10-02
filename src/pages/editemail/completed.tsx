c99bf837ac8cef94c86938aea54b124d04d9e506
Jovan Popovic <j.popovic@medii.jp>
2023-10-02 09:13:21 +0900

    /editemail/completedというページにもアクセス可能なため、修正

--- src/pages/editemail/completed.tsx
+++ src/pages/editemail/completed.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import EditEmail from '../../features/mypages/editEmail/EditEmail';
+import { useUpdateEmail } from '@/features/mypages/editEmail/useUpdateEmail';
 
 const Completed = () => {
+  const updateEmail = useUpdateEmail();
+  const { editEmailStatus, ...props } = updateEmail;
+
   return (
-    <div className="px-[84px]">
-      <h2 className="text-bold text-[24px]">
-        まだアドレス変更は完了しておりません。
-        <br />
-        新しいメールアドレスにメールをお送りしましたので、メールに記載のURLより、アドレス変更を完了してください。
-      </h2>
+    <div className="flex h-[100vh] bg-bg">
+      <div className="m-auto mt-9 w-[662px] rounded border border-[#ddd] bg-white py-[60px] text-left">
+      {!editEmailStatus ? <EditEmail {...props} /> : 
+      <div className="px-[84px]">
+        <h2 className="text-bold text-[24px]">
+          まだアドレス変更は完了しておりません。
+          <br />
+          新しいメールアドレスにメールをお送りしましたので、メールに記載のURLより、アドレス変更を完了してください。
+        </h2>
+      </div>
+    }
     </div>
+  </div>
   );
 };
 
--- src/pages/editemail/index.tsx
+++ src/pages/editemail/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import Completed from './completed';
-import EditEmail from '../../features/mypages/editEmail/EditEmail';
 import { MyPageLayout } from '@/components/Layouts/MyPageLayout';
-import { useUpdateEmail } from '@/features/mypages/editEmail/useUpdateEmail';
 
 const EditEmailPage = () => {
-  const updateEmail = useUpdateEmail();
-  const { editEmailStatus, ...props } = updateEmail;
-
   return (
-    <div className="flex h-[100vh] bg-bg">
-      <div className="m-auto mt-9 w-[662px] rounded border border-[#ddd] bg-white py-[60px] text-left">
-        {!editEmailStatus ? <EditEmail {...props} /> : <Completed />}
-      </div>
-    </div>
+        <Completed />
   );
 };
 
