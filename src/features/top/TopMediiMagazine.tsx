8aca287a62ba37160a1db1647e0168482ccacfb2
Shoichi Namba <alphabrend@gmail.com>
2023-10-05 15:19:04 +0900

    文言変更

--- src/features/top/TopMediiMagazine.tsx
+++ src/features/top/TopMediiMagazine.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Interview, TopInterview } from './TopInterview';
+import { Interview, TopMediiMagazineItem } from './TopMediiMagazineItem';
 
 const interviews: Interview[] = [
   {
-    title:
-      '若手医師が感じる経験不足をE-コンサルで補う。迷ったら、まず気軽に質問してみることが患者さんのためになる。',
+    title: '若手医師が感じる経験不足をE-コンサルで補う。迷ったら、まず気軽に質問してみることが患者さんのためになる。',
     url: 'https://note.com/medii_ecns/n/ndada7a1e5305?magazine_key=mc775b94f3819',
     medicalSpeciality: 'リウマチ膠原病内科医',
     doctorName: '多田先生',
@@ -12,8 +11,7 @@ const interviews: Interview[] = [
       'https://assets.st-note.com/production/uploads/images/93272671/rectangle_large_type_2_49ce1ce697e149f47bff0721306e1900.png?width=200',
   },
   {
-    title:
-      'オンラインで気軽に匿名相談できる手軽さや質問の返答が60分以内と早くて満足。',
+    title: 'オンラインで気軽に匿名相談できる手軽さや質問の返答が60分以内と早くて満足。',
     url: 'https://note.com/medii_ecns/n/n27ae8f034ecd?magazine_key=mc775b94f3819',
     medicalSpeciality: '総合診療内科',
     doctorName: '谷崎隆太郎先生',
@@ -22,23 +20,17 @@ const interviews: Interview[] = [
   },
 ];
 
-export const TopInterviews = () => {
+export const TopMediiMagazine = () => {
   return (
     <>
       <div className="flex items-center">
-        <p className="flex-grow text-xxl font-bold text-text-primary">
-          活用事例
-        </p>
-        <a
-          href="https://note.com/medii_ecns/m/mc775b94f3819"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <p className="flex-grow text-xxl font-bold text-text-primary">Medii Magazine</p>
+        <a href="https://medii.jp/magazine/tag/consultant" target="_blank" rel="noreferrer">
           <button className="text-md text-text-secondary">もっと見る</button>
         </a>
       </div>
       {interviews.map((interview) => (
-        <TopInterview key={interview.url} interview={interview} />
+        <TopMediiMagazineItem key={interview.url} interview={interview} />
       ))}
     </>
   );
