693a13efd242331effe5155650f0d5a215229989
Shoichi Namba <alphabrend@gmail.com>
2023-10-06 16:44:14 +0900

    上部の文言など調整

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -64,7 +64,7 @@ const Registration: NextPageWithLayout = () => {
             <div className="mx-auto flex justify-center md:py-4">
               <div
                 className="
-              mb-8 flex flex-col items-stretch justify-center md:flex-row md:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]
+              flex flex-col items-stretch justify-center md:flex-row md:shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] lg:mb-8
             "
               >
                 <img src="images/registration/left.png" className="hidden max-w-[480px] bg-white md:block" alt="" />
@@ -72,10 +72,14 @@ const Registration: NextPageWithLayout = () => {
                 <div
                   className="
                 flex max-w-[480px] flex-col items-center justify-items-stretch bg-white
-                px-4 pb-6 md:pb-4 md:pl-8 md:pr-12 md:pt-12
+                px-4 pb-6 pt-6 md:pb-4 lg:px-6 lg:pt-8
                 "
                 >
-                  <h1 className="my-6 text-center text-2xl text-primary">新規会員登録</h1>
+                  <h1 className="text-center text-2xl">新規会員登録</h1>
+                  <div className="mt-2 text-sm font-light lg:text-[16px]">※医師・医学生専用のサービスです</div>
+                  <p className="mt-6 hidden text-sm font-light lg:block">
+                    E-コンサルに会員登録すると、利用規約とプライバシーポリシーに同意したものとみなされます。
+                  </p>
                   <form
                     onSubmit={(e) => {
                       e.preventDefault();
@@ -92,67 +96,6 @@ const Registration: NextPageWithLayout = () => {
                       }}
                       required
                     />
-                    <p className="w-full">個人情報の取扱いについて</p>
-                    <div className="h-[238px] overflow-y-scroll rounded-sm border border-slate-400 p-4 leading-[160%]">
-                      株式会社Medii（以下当社）は、会員登録で取得する個人情報を以下の通り取扱います。
-                      <br />
-                      1. 利用目的
-                      <br />
-                      E-コンサルサービス提供のため（必要なアクセス権限の付与、確認を含む。なお、当社の各サービスにおける利用目的は各サービスの利用規約に表示）
-                      <br />
-                      2. 個人情報の第三者提供について
-                      <br />
-                      取得した個人情報は、本人からの事前の同意取得、又は法定の除外事由を除き第三者に提供することはありません。
-                      <br />
-                      ※会員医師がコンサルトを依頼する情報、およびメンター医師が回答する情報について、当社が独自の目的で利用することはありません。
-                      また、会員医師がメンター医師に対するコンサルト依頼に関連して行なう情報提供は、国のガイダンスで認められる範囲内に限られます。
-                      <br />
-                      3. 個人情報取扱の委託について
-                      <br />
-                      安定したサービスの提供のために、個人情報の取扱いを含む業務の一部を委託することがあります。
-                      <br />
-                      その際も情報の取扱い適格を有すると評価した者にのみ委託します。
-                      <br />
-                      4.個人情報の取扱いに関する請求について
-                      <br />
-                      個人情報により特定されるご本人から利用目的の通知、開示、内容の訂正、追加又は削除、利用の停止、消去及び第三者への提供の停止（以下「開示等」という）のお申し出があった場合は対応いたします。
-                      <br />
-                      個人情報保護管理者にお問い合わせください。
-                      <br />
-                      5. 任意性
-                      <br />
-                      当社への個人情報のご提供は任意ですが、ご提供いただけない場合、当社として会員登録その他サービスを適切に提供できなくなるおそれがあります。
-                      <br />
-                      6. お問い合わせ先
-                      <br />
-                      取締役個人情報保護管理者 e-mail: info@medii.jp
-                    </div>
-                    <div className="mt-2 w-full">
-                      <CheckBox
-                        name="privacyPolicy"
-                        label="個人情報の取り扱いについて同意します。"
-                        onChange={(e) => {
-                          setIsPrivacyPolicyAgree(e.target.checked);
-                        }}
-                      />
-                    </div>
-                    <div className="mt-2 w-full">
-                      <CheckBox
-                        name="termsAgree"
-                        label={
-                          <>
-                            {' '}
-                            <a href="https://e-consult.medii.jp/doc/terms_of_usage.pdf" className="text-[-webkit-link]">
-                              利用規約
-                            </a>
-                            の取り扱いについて同意します。
-                          </>
-                        }
-                        onChange={(e) => {
-                          setIsTermsAgree(e.target.checked);
-                        }}
-                      />
-                    </div>
                     {errorMessage != '' && <ErrorMessage className="mt-6 text-center">{errorMessage}</ErrorMessage>}
                     {isEmailDuplicated && (
                       <ErrorMessage className="text-center">
