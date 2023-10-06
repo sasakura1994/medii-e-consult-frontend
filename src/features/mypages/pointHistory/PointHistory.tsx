505d1d24e0bbf81a293b3a96aa4466e7ff8b445d
JovanPopovic0911 <j.popovic@medii.jp>
2023-10-06 00:49:38 -0700

    pointhistoryページでAPIを修正、細かい表示対応

--- src/features/mypages/pointHistory/PointHistory.tsx
+++ src/features/mypages/pointHistory/PointHistory.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import Link from 'next/link';
-import styles from './PointHistory.module.scss';
 import { dateFormat } from '@/libs/date';
 import { usePointHistory } from './usePointHisotry';
 
@@ -23,7 +21,7 @@ export const PointHistory: React.FC = () => {
   }
 
   return (
-    <div className={styles.point_history}>
+    <div className='w-100 bg-white mt-10 mb-[49px] lg:mb-10 rounded border-[1px] border-solid border-[#d5d5d5] p-4'>
       <div className="mb-6">
         <h2>Medii ポイント残高</h2>
         <p className="text-[28px]">
@@ -37,25 +35,25 @@ export const PointHistory: React.FC = () => {
           Medii ポイント履歴
         </h2>
 
-        <table className={styles.history_table}>
-          <thead>
-            <tr>
-              <th>アクション</th>
-              <th>日時</th>
-              <th>ポイント差分</th>
-              <th>備考</th>
+        <table className="border-separate">
+          <thead className='text-[#999999] text-left'>
+            <tr className='leading-4'>
+              <th className='pr-4'>アクション</th>
+              <th className='pr-4'>日時</th>
+              <th className='pr-4'>ポイント差分</th>
+              <th className='pr-4'>備考</th>
             </tr>
           </thead>
           <tbody>
             {pointHistories &&
               pointHistories.map((pointHistory) => (
-                <tr key={pointHistory.ref_id}>
-                  <td>{getActionNameFromRefId(pointHistory.ref_id)}</td>
-                  <td>
+                <tr key={pointHistory.uid}>
+                  <td className='text-[15px] py-1 pr-5'>{getActionNameFromRefId(pointHistory.ref_id)}</td>
+                  <td className='text-[15px] py-1 pr-5'>
                     {dateFormat(pointHistory.created_date, 'YYYY/M/D HH:mm')}
                   </td>
-                  <td className="text-right">{pointHistory.delta}</td>
-                  <td>
+                  <td className="text-right text-[15px] py-1 pr-5">{pointHistory.delta}</td>
+                  <td className='text-[15px] py-1 pr-5'>
                     {pointHistory.ref_id.startsWith('con') && (
                       <Link href={getChatRoomLink(pointHistory.ref_id)}>
                         <a className="text-[#0000ee] underline">
