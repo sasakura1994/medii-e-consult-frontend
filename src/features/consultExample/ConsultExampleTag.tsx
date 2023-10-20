8f1a5a6968e1763fa651576d97a75b560005fe79
JovanPopovic0911 <j.popovic@medii.jp>
2023-10-19 18:56:45 -0700

    /examplelistページからはみ出る - 修正

--- src/features/consultExample/ConsultExampleTag.tsx
+++ src/features/consultExample/ConsultExampleTag.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const ConsultExampleTag: React.FC<Props> = ({ children }: Props) => {
   return (
-    <div className="inline-block rounded bg-primary-light px-2 py-2 text-sm lg:px-4">
+    <div className="inline-block rounded bg-primary-light px-2 py-2 text-sm lg:px-4 max-w-[200px]">
       {children}
     </div>
   );
