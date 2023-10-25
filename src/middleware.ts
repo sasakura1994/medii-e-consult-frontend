b16cb4834664218b06cb265af85e8b4847d07b97
sasakura1994 <kanazawa01211123@gmail.com>
2023-10-25 12:54:23 +0900

    middleware.tsを復活させる

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse, NextRequest } from 'next/server';
+
+const Middleware = (req: NextRequest) => {
+  if (req.nextUrl.pathname.toLowerCase() !== req.nextUrl.pathname) {
+    const url = req.nextUrl.clone();
+    url.pathname = req.nextUrl.pathname.toLowerCase();
+
+    return NextResponse.rewrite(url);
+  }
+  return NextResponse.next();
+};
+
+export default Middleware;
