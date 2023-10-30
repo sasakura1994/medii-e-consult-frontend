50584f514127e5062de4ba2b4a991abf4c1ae78c
JovanPopovic0911 <j.popovic@medii.jp>
2023-10-29 18:24:30 -0700

    Update TopNewerConsults.tsx

--- src/features/top/TopNewerConsults.tsx
+++ src/features/top/TopNewerConsults.tsx
@@ -24,7 +24,6 @@ export const TopNewerConsults = () => {
         </div>
         <div className="flex px-4">
           <svg className='drop-shadow-popup' width="10" height="5" viewBox="0 0 10 5" fill="none" xmlns="http://www.w3.org/2000/svg">
-            
             <path  d="M5 5L0 0L10 0L5 5Z" fill="white" />
           </svg>
         </div>
