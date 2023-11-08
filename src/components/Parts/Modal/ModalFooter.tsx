88179b9dc083c0f6206dfb4994269b6476c99024
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-08 11:34:06 +0900

    処理をuseModalFooterにまとめる

--- src/components/Parts/Modal/ModalFooter.tsx
+++ src/components/Parts/Modal/ModalFooter.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import React, { ReactNode } from 'react';
+import { useModalFooter } from './useModalFooter';
 
 export type ModalFooterProps = {
   isUseFooter?: boolean;
@@ -8,37 +9,13 @@ export type ModalFooterProps = {
 
 export const ModalFooter = (props: ModalFooterProps) => {
   const { isUseFooter = false, closeButton, submitButton } = props;
-  const closeButtonRef = useRef<HTMLDivElement>(null);
-  const submitButtonRef = useRef<HTMLDivElement>(null);
-  const [flexDirection, setFlexDirection] = useState<'flex-row' | 'flex-col-reverse'>('flex-row');
-
-  useEffect(() => {
-    // ボタンの幅をチェックし、条件に基づいてflex-directionを設定する
-    const checkButtonWidth = () => {
-      const closeBtnWidth = closeButtonRef.current ? closeButtonRef.current.offsetWidth : 0;
-      const submitBtnWidth = submitButtonRef.current ? submitButtonRef.current.offsetWidth : 0;
-
-      // 閾値は320px
-      const maxWidth = 320;
-
-      if (closeBtnWidth > maxWidth || submitBtnWidth > maxWidth) {
-        setFlexDirection('flex-col-reverse');
-      } else {
-        setFlexDirection('flex-row');
-      }
-    };
-    window.addEventListener('resize', checkButtonWidth);
-    checkButtonWidth();
-    return () => {
-      window.removeEventListener('resize', checkButtonWidth);
-    };
-  }, []);
+  const { flexDirection, closeButtonRef, submitButtonRef } = useModalFooter();
 
   if (!isUseFooter) return <></>;
   return (
     <div className={'flex justify-center gap-2 self-stretch p-4 lg:justify-end ' + flexDirection}>
-      <div ref={closeButtonRef}>{closeButton}</div>
-      <div ref={submitButtonRef}>{submitButton}</div>
+      {closeButton && <div ref={closeButtonRef}>{closeButton}</div>}
+      {submitButton && <div ref={submitButtonRef}>{submitButton}</div>}
     </div>
   );
 };
