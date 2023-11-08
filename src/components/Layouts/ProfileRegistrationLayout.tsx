9531c3590e3944d9307752c2c3650b6e7f2f1496
shoichi namba <alphabrend@gmail.com>
2023-11-08 11:47:28 +0900

    レイアウトを更新

--- src/components/Layouts/ProfileRegistrationLayout.tsx
+++ src/components/Layouts/ProfileRegistrationLayout.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Header } from '@/components/Layouts/Header/Header';
+import { HeaderLogoOnly } from './Header/HeaderLogoOnly';
 
 type PropsType = {
   children: React.ReactNode;
 };
 
-export const MyPageLayoutWithoutSpFooterMenu = (props: PropsType) => {
+export const ProfileRegistrationLayout = (props: PropsType) => {
   const { children } = props;
 
   return (
     <div className="h-full min-h-screen w-full bg-bg">
-      <Header />
+      <HeaderLogoOnly />
       <main className="mx-auto pb-20 pt-10 lg:w-lg-breakpoint lg:pb-0">{children}</main>
     </div>
   );
