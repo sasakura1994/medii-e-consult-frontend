2be7f87d4ca730404f0f3ccc6c26314a02fcf628
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-08 12:52:47 +0900

    新コンポーネントをコピーしてくる

--- src/components/Parts/Modal/Modal.tsx
+++ src/components/Parts/Modal/Modal.tsx
@@ -1,36 +1,40 @@
 import React from 'react';
 import { useModal } from './useModal';
+import { ModalFooter, type ModalFooterProps } from './ModalFooter';
 
 export type ModalPropsType = {
   children: React.ReactNode;
+  pcWidth?: '600' | '400';
   className?: string;
   isCenter?: boolean;
   setShowModal?: (isShow: boolean) => void;
   dataTestId?: string;
-};
+} & ModalFooterProps;
 
 export const Modal: React.FC<ModalPropsType> = (props) => {
-  const { children, className, dataTestId } = props;
+  const { isCenter, children, pcWidth = '600', className, dataTestId, ...ModalFooterProps } = props;
   const { hideModal, modalRef } = useModal(props);
-
+  const pcWidthClass = `w-full lg:w-[${pcWidth}px]`;
   return (
     <div
       ref={modalRef}
-      className={`modal fixed left-0 top-0 z-[200] w-screen overflow-y-auto bg-bg-overlay ${
-        props.isCenter === true ? 'flex items-center justify-center' : ''
+      className={`fixed left-0 top-0 z-[200] w-screen overflow-y-auto bg-bg-overlay ${
+        isCenter === true ? 'flex items-center justify-center' : ''
       }`}
       style={{ height: '100dvh' }}
       onMouseDown={hideModal}
       data-testid={dataTestId}
     >
       <div
-        className={`rounded border border-[#d5d5d5] bg-white ${className} z-[210] mx-auto ${
-          props.isCenter === true ? '' : 'my-10'
-        }`}
+        className={`mx-4 my-6 rounded-lg border border-[#d5d5d5] bg-white ${className} z-[210] ${
+          isCenter === true ? '' : 'my-10'
+        }
+         ${pcWidthClass}`}
         role="dialog"
         onMouseDown={(e) => e.stopPropagation()}
       >
         {children}
+        <ModalFooter {...ModalFooterProps} />
       </div>
     </div>
   );
---
+++ src/components/TextArea/TextArea.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode } from 'react';
+
+type TextAreaProps = {
+  labelText?: string;
+  labelBadge?: ReactNode;
+  disabled?: boolean;
+  className?: string;
+  name?: string;
+  id: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  subText?: string;
+  required?: boolean;
+  dataTestId?: string;
+  hasError?: boolean;
+};
+
+const TextArea = (props: TextAreaProps) => {
+  const {
+    labelText,
+    labelBadge,
+    name,
+    id,
+    disabled,
+    className,
+    placeholder,
+    value,
+    onChange,
+    subText,
+    required,
+    dataTestId,
+    hasError = false,
+  } = props;
+  return (
+    <>
+      <label htmlFor={id} className="mb-2 flex items-center gap-x-2">
+        <p className="text-base font-semibold text-text-primary">{labelText}</p>
+        {labelBadge}
+      </label>
+      <textarea
+        id={id}
+        name={name}
+        disabled={disabled}
+        className={`rounded-lg ${
+          hasError
+            ? 'border-2 border-alert focus:border-alert focus:outline-alert'
+            : 'border border-border-field focus:border-border-selected'
+        } px-3 py-2 text-md
+        disabled:border-border-disabled disabled:bg-bg-secondary disabled:text-text-disabled
+         ${className}`}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        data-testid={dataTestId}
+        required={required}
+      />
+      <p className="text-medii-sm text-text-secondary">{subText}</p>
+    </>
+  );
+};
+
+export default TextArea;
