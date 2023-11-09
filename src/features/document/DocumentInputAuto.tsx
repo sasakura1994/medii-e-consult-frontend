6da7577f278ad37b580ea2017c86781ceb2b2ad3
shoichi namba <alphabrend@gmail.com>
2023-11-09 14:22:21 +0900

    Mediiにおまかせ調整

--- src/features/document/DocumentInputAuto.tsx
+++ src/features/document/DocumentInputAuto.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 import { useDocumentInputAuto } from './useDocumentInputAuto';
 import { DocumentSelected } from '.';
-import { YearInput } from '@/components/Parts/Form/YearInput';
 import PrimaryButton from '@/components/Button/PrimaryButton';
+import SecondaryButton from '@/components/Button/SecondaryButton';
 
 type DocumentInputAutoProps = {
   setSelectedWithRedirect: (value: DocumentSelected) => void;
 };
 
 const DocumentInputAuto: React.FC<DocumentInputAutoProps> = ({ setSelectedWithRedirect }) => {
-  const { errorMessage, eraConverter, submit, getPrefectureNameByCode, hospital, profile, setTel, tel, year, setYear } =
-    useDocumentInputAuto({ setSelectedWithRedirect });
+  const { errorMessage, submit, getPrefectureNameByCode, hospital, profile } = useDocumentInputAuto({
+    setSelectedWithRedirect,
+  });
   return (
     <form
       onSubmit={(e) => {
@@ -20,76 +21,28 @@ const DocumentInputAuto: React.FC<DocumentInputAutoProps> = ({ setSelectedWithRe
       }}
       data-testid="document-input-auto"
     >
-      <div
-        className="border-1 rounded-xs -mb-10 mt-5 w-full border bg-white
-       px-6 pb-10 pt-4 lg:mb-0 lg:mt-10 lg:px-20"
-      >
-        <div className="relative mt-7 flex text-left text-2xl font-bold lg:mt-7 lg:text-center">
-          <div className="hidden cursor-pointer lg:block">
-            <img src="icons/arrow_left.svg" className="mt-1.5 h-3 w-3" alt="arrow_left" />
-            <div
-              className="absolute left-0 top-0 pl-4 text-base"
-              onClick={() => {
-                setSelectedWithRedirect('');
-              }}
-            >
-              選択へ戻る
-            </div>
-          </div>
-          <div className="mx-auto mb-10">Mediiにおまかせ</div>
-        </div>
-        <div className="lg:text-center">入力された情報を元にMediiが医師資格確認を行います。</div>
-        <div className="mt-1 lg:text-center">
-          上記確認作業で医師資格をご確認できなかった場合は、Mediiから勤務先に在籍確認のご連絡を差し上げる場合がございます。
-        </div>
-        <div className="flex justify-center">
-          <div className="mt-2 text-left text-sm">※できる限りご本人に転送されるように致します。</div>
-        </div>
-        <div className="mt-10 text-center  font-bold lg:text-left">ご連絡先</div>
-        <div className="mt-6 text-left font-bold">勤務先病院名</div>
-        <div className="text-left">{hospital?.hospital_name ?? profile?.hospital_name}</div>
-        <div className="mt-4 text-left font-bold">勤務先病院の所在地</div>
-        <div className="text-left">{getPrefectureNameByCode(profile?.prefecture_code)}</div>
-        <div className="mt-6 flex ">
-          <div className="mr-1 rounded-md border border-primary px-1 py-0.5 text-xs font-bold text-primary">任意</div>
-          <div className="text-left font-bold">勤務先電話番号</div>
-        </div>
-        <div className="mt-3">
-          <input
-            value={tel}
-            className="h-12 w-full rounded-md border border-gray-400 px-2"
-            onChange={(e) => {
-              setTel(e.target.value);
-            }}
-            type="tel"
-            pattern="\d{2,4}-?\d{2,4}-?\d{3,4}"
-            placeholder="連絡可能な電話番号を入力してください"
-          />
-        </div>
-        {profile?.doctor_qualified_year === 9999 && (
-          <>
-            <div className="mt-6 flex text-left">
-              <div className="mr-1 rounded-md border border-red-500 px-1 py-0.5 text-xs font-bold text-red-500">
-                必須
-              </div>
-              <div className="text-left font-bold">医師免許取得年</div>
-            </div>
-            <div className="mt-2 text-left text-sm text-gray-500">（例）西暦：2022年 和暦：令和4年</div>
-            <div className="mt-1">
-              <YearInput {...eraConverter} value={year} onChange={setYear} />
-            </div>
-          </>
-        )}
+      <div className="-mb-10 mt-8 w-full px-4 lg:mb-0 lg:w-[600px] lg:px-0">
+        <div className="text-xl font-semibold">Mediiにおまかせ</div>
+        <ul className="mt-2 list-disc pl-6">
+          <li>Mediiスタッフが登録情報を元に医師資格確認を行います。医師確認できた場合、ここで完了となります。</li>
+          <li>
+            万が一確認できなかった場合、「医師免許証の登録」または「医師番号の登録」から再度お手続きをお願いします。
+          </li>
+        </ul>
+        <div className="mt-8 font-semibold">勤務先病院名</div>
+        <div className="mt-2">{hospital?.hospital_name ?? profile?.hospital_name}</div>
+        <div className="mt-6 font-semibold">所在地</div>
+        <div className="mt-2">{getPrefectureNameByCode(profile?.prefecture_code)}</div>
+        <div className="mt-6 font-semibold">医師免許取得年</div>
+        <div className="mt-2">{profile && profile.qualified_year !== 9999 ? profile.qualified_year : ''}年</div>
       </div>
       {errorMessage && <div className="mt-5 text-center text-base font-bold text-red-400">{errorMessage}</div>}
-      <div className="-mb-20 mt-7 flex justify-center lg:mb-0 lg:mt-0">
-        <PrimaryButton
-          type="submit"
-          size='large'
-          className='my-10 pb-2 pt-1.5 shadow-lg'
-          disabled={profile?.doctor_qualified_year === 9999 && year === 0}
-        >
-          登録を完了する
+      <div className="mt-8 flex justify-center gap-2">
+        <SecondaryButton onClick={() => setSelectedWithRedirect('')} size="large">
+          選択へ戻る
+        </SecondaryButton>
+        <PrimaryButton type="submit" size="large">
+          Mediiにおまかせで登録
         </PrimaryButton>
       </div>
     </form>
