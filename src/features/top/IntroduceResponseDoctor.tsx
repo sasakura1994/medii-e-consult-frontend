301b55af4a1ea772cc9ca16ebe3600bf8c584d40
JovanPopovic0911 <j.popovic@medii.jp>
2023-11-09 17:25:48 +0900

    120文字を超えているので修正

--- src/features/top/IntroduceResponseDoctor.tsx
+++ src/features/top/IntroduceResponseDoctor.tsx
@@ -10,7 +10,7 @@ export const IntroduceResponseDoctor = () => {
   return (
     <>
       <div 
-        className='flex' 
+        className="flex" 
         onClick={() => {
           postEventLog({ name: 'click-doctor' })
         }}
@@ -23,16 +23,16 @@ export const IntroduceResponseDoctor = () => {
               key={index}
             >
               <div className="p-4">
-                <div className='flex'>
-                  <div className='mr-4 w-[70px]'>
+                <div className="flex">
+                  <div className="mr-4 w-[70px]">
                     <img src={introduceDoctor.image} alt="introduceDoctorImage" />
                   </div>
                   <div>
-                    <div className='flex mb-2 items-center'>
-                      <p className='text-lg font-semibold'>{introduceDoctor.name}</p>
-                      <p className='ml-2 text-sm font-light'>先生</p>
+                    <div className="flex mb-2 items-center">
+                      <p className="text-lg font-semibold">{introduceDoctor.name}</p>
+                      <p className="ml-2 text-sm font-light">先生</p>
                     </div>
-                    <p className='text-sm font-semibold'>
+                    <p className="text-sm font-semibold">
                       {introduceDoctor.job}
                       <br />
                       {introduceDoctor.guideline}
@@ -40,14 +40,21 @@ export const IntroduceResponseDoctor = () => {
                     </p>
                   </div>
                 </div>
-                <div className='mt-1 mb-2'>
-                  <img src="images/top/triangle.png" alt="triangle" className='relative z-1 top-[1px]' />
-                  <div className='bg-bg-secondary border border-[1px] border-border-divider rounded-lg p-[10px] font-light text-xs gap-2.5'>
+                <div className="mt-1 mb-2">
+                  <img 
+                    src="images/top/triangle.png" 
+                    alt="triangle" 
+                    className="relative z-1 top-[1px]" 
+                  />
+                  <div 
+                    className="bg-bg-secondary border border-[1px] border-border-divider rounded-lg 
+                      p-[10px] font-light text-xs gap-2.5"
+                  >
                     {introduceDoctor.description}
                   </div>
                 </div>
                 <div 
-                  className='flex text-medii-blue-700 mb-3 cursor-pointer text-xs' 
+                  className="flex text-medii-blue-700 mb-3 cursor-pointer text-xs" 
                   onClick={
                     (e) => { 
                       e.preventDefault();
@@ -56,7 +63,7 @@ export const IntroduceResponseDoctor = () => {
                   }
                 >
                   <p>先生の情報をもっと見る</p>
-                  <div className='flex items-center pl-2'>
+                  <div className="flex items-center pl-2">
                     {showMore[index] ?
                       <img src="images/top/doctor-information-more.svg" alt="doctor-information-more" /> :
                       <img src="images/top/doctor-information-less.svg" alt="doctor-information-less" />}
@@ -65,36 +72,48 @@ export const IntroduceResponseDoctor = () => {
                 {showMore[index] &&
                   <div>
                     {introduceDoctor.university &&
-                      <div className='mb-3'>
-                        <p className='text-base font-semibold'>出身大学（卒年）</p>
-                        <p className='text-xs font-light'>{introduceDoctor.university}</p>
+                      <div className="mb-3">
+                        <p className="text-base font-semibold">出身大学（卒年）</p>
+                        <p className="text-xs font-light">{introduceDoctor.university}</p>
                       </div>
                     }
                     {introduceDoctor.field &&
-                      <div className='mb-3'>
-                        <p className='text-base font-semibold'>専門分野</p>
-                        <p className='text-xs font-light'>{introduceDoctor.field}</p>
+                      <div className="mb-3">
+                        <p className="text-base font-semibold">専門分野</p>
+                        <p className="text-xs font-light">{introduceDoctor.field}</p>
                       </div>
                     }
                     {introduceDoctor.qualification &&
-                      <div className='mb-3'>
-                        <p className='text-base font-semibold'>学会認定・資格</p>
-                        <ul className='text-xs font-light'>
+                      <div className="mb-3">
+                        <p className="text-base font-semibold">学会認定・資格</p>
+                        <ul className="text-xs font-light">
                           {introduceDoctor.qualification.map((qualifications, key) => {
                             return (
-                              <li key={key} className="pl-7 before:mt-[-2px] before:absolute before:text-[4pt] before:pt-1 before:ml-[-18pt] before:content-['\25CF']">{qualifications.content}</li>
+                              <li 
+                                key={key} 
+                                className="pl-7 before:mt-[-2px] before:absolute before:text-[4pt] before:pt-1 
+                                  before:ml-[-18pt] before:content-['\25CF']"
+                              >
+                                {qualifications.content}
+                              </li>
                             )
                           })}
                         </ul>
                       </div>
                     }
                     {introduceDoctor.book &&
-                      <div className='mb-3'>
-                        <p className='text-base font-semibold'>著書等</p>
-                        <ul className='text-xs font-light'>
+                      <div className="mb-3">
+                        <p className="text-base font-semibold">著書等</p>
+                        <ul className="text-xs font-light">
                           {introduceDoctor.book.map((books, index) => {
                             return (
-                              <li key={index} className="pl-7 before:mt-[6px] before:absolute before:text-[4pt] before:pt-1 before:ml-[-18pt] before:content-['\25CF']">{books.content}</li>
+                              <li 
+                                key={index} 
+                                className="pl-7 before:mt-[6px] before:absolute before:text-[4pt] before:pt-1 
+                                  before:ml-[-18pt] before:content-['\25CF']"
+                              >
+                                {books.content}
+                              </li>
                             )
                           })}
                         </ul>
