2d11d7bb55891f467d5cbdf719da9bf8c9dfc65c
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-13 15:14:31 +0900

    test

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -64,6 +64,15 @@ const TextArea = (props: TextAreaProps) => {
          ${className}`}
         value={value}
         onChange={onChange}
+
+
+
+
+
+
+
+
+        
         placeholder={placeholder}
         data-testid={dataTestId}
         required={required}
