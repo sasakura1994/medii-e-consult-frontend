9029f23ccc7c6a07513b7c48c70d1be3b3739ba4
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-15 11:54:38 +0900

    専門家未指定の医師も表示させる

--- src/features/createGroup/InviteMemberModal.tsx
+++ src/features/createGroup/InviteMemberModal.tsx
@@ -198,6 +198,70 @@ export const InviteMemberModal = (props: Props) => {
                         </>
                       );
                     })}
+                  {/* 専門家未指定の医師一覧 */}
+                  <>
+                    {searchedMember.some((member) => {
+                      return member.speciality_code === '';
+                    }) && (
+                      <>
+                        <thead className="table-header-group text-left">
+                          <tr className="my-2 text-lg font-bold">専門家未指定</tr>
+                          <tr className="table-row border-y border-y-heading-line text-block-gray">
+                            <th className="table-cell whitespace-nowrap py-3 font-normal">エリア・施設名</th>
+                            <th className="table-cell whitespace-nowrap py-3 font-normal">氏名</th>
+                            <th className="table-cell whitespace-nowrap py-3 font-normal">勤務先病院</th>
+                            <th className="sticky right-0 table-cell whitespace-nowrap bg-white py-3 font-normal">
+                              招待
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className="table-row-group">
+                          {searchedMember
+                            .filter((member) => {
+                              return !member.speciality_code;
+                            })
+                            .map((member) => {
+                              return (
+                                <label
+                                  htmlFor={'invite' + member.account_id}
+                                  key={member.account_id}
+                                  className="table-row cursor-pointer overflow-scroll hover:bg-primary-light"
+                                  onClick={() => {}}
+                                >
+                                  <td className="table-cell py-3">{member.area_txt}</td>
+                                  <td className="table-cell py-3">{member.full_name}</td>
+                                  <td className="table-cell py-3">{member.hospital_name}</td>
+                                  <td className="sticky right-0 table-cell cursor-pointer bg-white px-2 py-3">
+                                    {selectedMembers.some((m) => m.account_id === member.account_id) ? (
+                                      <p className="text-sm text-[#808080]">招待済み</p>
+                                    ) : (
+                                      <input
+                                        type="checkbox"
+                                        className="bg-medii-blue-base"
+                                        name={'invite' + member.account_id}
+                                        id={'invite' + member.account_id}
+                                        onChange={(e) => {
+                                          if (
+                                            e.target.checked &&
+                                            !checkedMemberRef.current.some((m) => m.account_id === member.account_id)
+                                          ) {
+                                            checkedMemberRef.current.push(member);
+                                            return;
+                                          }
+                                          checkedMemberRef.current = checkedMemberRef.current.filter(
+                                            (m) => m.account_id !== member.account_id
+                                          );
+                                        }}
+                                      />
+                                    )}
+                                  </td>
+                                </label>
+                              );
+                            })}
+                        </tbody>
+                      </>
+                    )}
+                  </>
                 </table>
               </div>
             </div>
