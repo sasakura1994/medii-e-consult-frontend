d665ffe330d5781a1bcf24f38afa47191afa5b4b
JovanPopovic0911 <j.popovic@medii.jp>
2023-11-16 10:15:45 +0900

    statusコード削除、setTokenAndMarkInitializedでtokenを設定、tailwindCss修正

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useFetchAppleAuthGetToken } from '@/hooks/api/auth/useFetchAppleAuthGetToken';
-import { setAuthToken } from '@/libs/cookie';
 import { useLogin } from '@/hooks/useLogin';
+import { useToken } from '@/hooks/authentication/useToken';
 
 type Query = {
   token: string;
@@ -12,19 +12,17 @@ const AuthPage = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const router = useRouter();
   const { redirectUrl } = useLogin();
+  const { setTokenAndMarkInitialized } = useToken();
   const { token } = router.query as Query;
   const { fetchAppleAuthGetToken } = useFetchAppleAuthGetToken();
 
   const login = useCallback(async () => {
     fetchAppleAuthGetToken({ token_id: token })
       .then((res) => {
-        if (res.status === 200) {
-          const { jwt_token, login_type } = res.data;
-          // ここでcookieにjwt_tokenをセットする
-          setAuthToken(jwt_token);
-          if (login_type==="register") router.push(redirectUrl === '' ? 'top' : redirectUrl);
-          else router.push('editprofile?registerMode=1');
-        }
+        const { jwt_token, login_type } = res.data;
+        setTokenAndMarkInitialized(jwt_token);
+        if (login_type==="register") router.push(redirectUrl === '' ? 'top' : redirectUrl);
+        else router.push('editprofile?registerMode=1');
       })
       .catch((err) => {
         console.log(err);
@@ -37,12 +35,10 @@ const AuthPage = () => {
   }, [login]);
 
   return (
-    <div>
-      <div>
-        <div style={{ marginBottom: '48px', textAlign: 'center' }}>
-          <div>loading...</div>
-          <p>{errorMessage}</p>
-        </div>
+    <div className="flex h-screen justify-center bg-bg">
+      <div className="mb-12 mt-6">
+        <div>loading...</div>
+        <p>{errorMessage}</p>
       </div>
     </div>
   );
