a7c9a1a787a056930099a428e006c59600153f90
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-17 15:53:10 +0900

    コミット漏れ

--- src/components/Layouts/Header/HeaderSimple.tsx
+++ src/components/Layouts/Header/HeaderSimple.tsx
@@ -3,14 +3,12 @@ import { HeaderContainer } from './HeaderContainer';
 import { HeaderLogo } from './HeaderLogo';
 import { HeaderMenuList } from './HeaderMenuList';
 import { HeaderMenuListItem } from './HeaderMenuListItem';
-import { useToken } from '@/hooks/authentication/useToken';
 
 export const HeaderSimple = () => {
-  const { token } = useToken();
   return (
     <HeaderContainer>
       <div className="flex items-center">
-        <HeaderLogo href={token ? '/top' : '/login'} />
+        <HeaderLogo />
         <HeaderMenuList>
           <HeaderMenuListItem href="/login">ログイン</HeaderMenuListItem>
         </HeaderMenuList>
