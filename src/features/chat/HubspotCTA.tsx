091af3d062bc86a19c446cfd59ec45342aa6d330
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-29 15:43:52 +0900

    api版

--- src/features/chat/HubspotCTA.tsx
+++ src/features/chat/HubspotCTA.tsx
@@ -1,57 +1,52 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useRef } from 'react';
 export const HubspotCTA = () => {
-  useEffect(() => {
-    const iframe = document.querySelector('iframe.go812842568') as HTMLIFrameElement;
-    if (iframe) {
-      console.log('iframe', iframe);
-
-      iframe.onload = () => {
-        if (iframe.contentWindow) {
-          console.log('iframe.contentWindow', iframe.contentWindow);
-
-          const iframeWindow = iframe.contentWindow.document.getElementById('id_color');
-          console.log('iframeWindow', iframeWindow);
-        }
-      };
-    }
-  }, []);
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//js.hs-scripts.com/43899841.js';
-    script.async = true;
-    script.defer = true;
-    script.id = 'hs-script-loader';
-    document.body.appendChild(script);
-  }, []);
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const portalId = '43899841';
+  const formGuid = '08c21a97-0a22-4b63-a56b-8f1863b78f29';
+  const config = {
+    headers: {
+      'Message-Type': 'application/json',
+    },
+  };
   return (
     <>
-      <div
-        className="hs-cta-embed hs-cta-embed-147691166006"
-        style={{ maxWidth: '100%', maxHeight: '100%', width: '270px', height: '578.0390625px' }}
-        data-hubspot-wrapper-cta-id="147691166006"
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await axios.post(
+            `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}`,
+            {
+              portalId,
+              formGuid,
+              fields: [
+                {
+                  name: 'firstname',
+                  value: firstNameRef.current?.value,
+                },
+              ],
+            },
+            config
+          );
+        }}
       >
-        <div className="hs-cta-loading-dot__container">
-          <div className="hs-cta-loading-dot"></div>
-          <div className="hs-cta-loading-dot"></div>
-          <div className="hs-cta-loading-dot"></div>
-        </div>
-        <div className="hs-cta-embed__skeleton"></div>
-        <picture>
-          <source
-            srcSet="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-            media="(max-width: 480px)"
-          />
-          <img
-            alt="Form CTA"
-            loading="lazy"
-            src="https://no-cache.hubspot.com/cta/default/43899841/interactive-147691166006.png"
-            style={{ height: '100%', width: '100%', objectFit: 'fill' }}
-            onError={(e) => {
-              (e.target as HTMLImageElement).style.display = 'none';
+        <label>
+          名前
+          <input
+            className="rounded-sm border border-gray-300"
+            type="text"
+            ref={firstNameRef}
+            onChange={(e) => {
+              if (firstNameRef.current) {
+                firstNameRef.current.value = e.target.value;
+              }
             }}
           />
-        </picture>
-      </div>
+        </label>
+        <button type="submit" value="Submit">
+          送信
+        </button>
+      </form>
     </>
   );
 };
