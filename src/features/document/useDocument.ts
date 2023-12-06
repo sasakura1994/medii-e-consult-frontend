2547c31138ef6fb82d8a33bcffe002497635be22
shoichi namba <alphabrend@gmail.com>
2023-12-06 18:49:20 +0900

    アンケート回答済みの場合はトップページへ

--- src/features/document/useDocument.ts
+++ src/features/document/useDocument.ts
@@ -4,6 +4,7 @@ import { useCallback, useState } from 'react';
 import { DocumentMode } from './RegistrationProgress';
 import { DocumentSelected } from '.';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
+import { useFetchFlag } from '@/hooks/api/account/useFetchFlags';
 
 const loginRedirectUrlKey = 'Login::RedirectURL';
 
@@ -13,11 +14,7 @@ export const useDocument = () => {
   const router = useRouter();
   const { postEventLog } = useEventLog();
   const { getItem, removeItem } = useLocalStorage();
-
-  const routerPushToQuestionaryPage = useCallback(async () => {
-    await postEventLog({ name: 'document-complete' });
-    router.push('/onboarding/questionary');
-  }, [postEventLog, router]);
+  const { flag: isOnboardingAnswered } = useFetchFlag('OnboardingAnswered');
 
   const setSelectedWithRedirect = useCallback(
     async (value: DocumentSelected) => {
@@ -25,14 +22,23 @@ export const useDocument = () => {
       if (value === 'completed') {
         const savedRedirectUrl = getItem(loginRedirectUrlKey);
         if (savedRedirectUrl && savedRedirectUrl !== '' && savedRedirectUrl.toLocaleLowerCase() !== '/top') {
+          removeItem(loginRedirectUrlKey);
           router.push(savedRedirectUrl);
-        } else {
-          await routerPushToQuestionaryPage();
+          return;
         }
+
         removeItem(loginRedirectUrlKey);
+        await postEventLog({ name: 'document-complete' });
+
+        if (isOnboardingAnswered === true) {
+          router.push('/top');
+          return;
+        }
+
+        router.push('/onboarding/questionary');
       }
     },
-    [getItem, removeItem, router, routerPushToQuestionaryPage]
+    [getItem, isOnboardingAnswered, postEventLog, removeItem, router]
   );
 
   return {
