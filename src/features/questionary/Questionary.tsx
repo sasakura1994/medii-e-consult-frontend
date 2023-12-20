ec811a0dd5f67bbb81b9a4b4dc80c14b86d0f527
sasakura1994 <kanazawa01211123@gmail.com>
2023-12-20 14:12:42 +0900

    HubSpotアンケートフォームの埋込

--- src/features/questionary/Questionary.tsx
+++ src/features/questionary/Questionary.tsx
@@ -1,5 +1,7 @@
+import { useToken } from '@/hooks/authentication/useToken';
+import { useProfile } from '@/hooks/useProfile';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HubspotForm from 'react-hubspot-form';
 
 type Query = {
@@ -8,13 +10,57 @@ type Query = {
 
 export const Questionary = () => {
   const router = useRouter();
-  const { id: formId } = router.query as Query;
-  if (!process.env.HUBSPOT_PORTAL_ID || !formId) {
+  const { accountId } = useToken();
+  const { email } = useProfile();
+  const { id: fromId } = router.query as Query;
+  const [isFormReady, setIsFormReady] = useState(false);
+
+  useEffect(() => {
+    if (!email || !accountId || !isFormReady) return;
+
+    const iframeElement = window.document.getElementsByTagName('iframe')[0];
+    if (!iframeElement.contentDocument) return;
+
+    // accountidとemailを設定する（この要素名でないとアンケートフォームに埋め込むことはできない）
+    const accountIdInput = iframeElement.contentDocument.querySelector('input[name="accountid"].hs-input');
+    const chatRoomIdInput = iframeElement.contentDocument.querySelector('input[name="email"].hs-input');
+
+    if (accountIdInput && chatRoomIdInput) {
+      accountIdInput.setAttribute('value', accountId);
+      chatRoomIdInput.setAttribute('value', email.mail_address);
+    }
+  }, [accountId, isFormReady, email]);
+
+  if (!process.env.HUBSPOT_PORTAL_ID || !fromId) {
     return <></>;
   }
   return (
-    <div className="m-4">
-      <HubspotForm portalId={process.env.HUBSPOT_PORTAL_ID} formId={formId} />
+    <div className="m-4 lg:mx-24">
+      <HubspotForm
+        portalId={process.env.HUBSPOT_PORTAL_ID}
+        formId={fromId}
+        onFormSubmitted={(
+          _: HTMLFormElement,
+          data: { redirectUrl: ''; submissionValues: { [key: string]: string | number | boolean | [] } }
+        ) => {
+          setTimeout(() => {
+            // if_consultが存在し、かつ下記形式ではないとうまく動作しないので注意
+            // if_consult: 1: 相談したい, 2: どちらともいえない, 3: 相談したくない
+            if (data.submissionValues.if_consult === '1' || data.submissionValues.if_consult === '2') {
+              router.push('/newchatroom');
+            } else {
+              router.push('/top');
+            }
+          }, 5000);
+        }}
+        onReady={(form: Element) => {
+          if (form) {
+            setTimeout(() => {
+              setIsFormReady(true);
+            }, 500);
+          }
+        }}
+      />
     </div>
   );
 };
--- src/pages/questionary/index.tsx
+++ src/pages/questionary/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Questionary } from '@/features/questionary/Questionary';
 
 const QuestionaryPage = () => {
-  <Questionary />;
+  return <Questionary />;
 };
 
 export default QuestionaryPage;
