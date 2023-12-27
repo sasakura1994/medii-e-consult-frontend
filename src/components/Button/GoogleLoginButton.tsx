087f188448bba5ed62bb72d34e9104c36bbf36b2
JovanPopovic0911 <j.popovic@medii.jp>
2023-12-27 23:46:57 +0900

    divide into components

--- src/components/Button/GoogleLoginButton.tsx
+++ src/components/Button/GoogleLoginButton.tsx
@@ -1,84 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
-import { useGoogleLogin } from '@/hooks/api/doctor/useGoogleLogin';
-import { mutateFetchProfile } from '@/hooks/api/doctor/useFetchProfile';
-import { useToken } from '@/hooks/authentication/useToken';
-import { loginRedirectUrlKey } from '@/data/localStorage';
-import { useRouter } from 'next/router';
-import { useLogin } from '@/hooks/useLogin';
-import { CredentialResponse } from '@react-oauth/google';
+import { useGoogleLoginButton } from './useGoogleLoginButton';
 
 type GoogleRegisterProps = {
   googleRegister?: boolean;
 };
 
-const LoginButton = ({ googleRegister }: GoogleRegisterProps) => {
-  const { googleLogin } = useGoogleLogin();
-  const { setTokenAndMarkInitialized } = useToken();
-  const router = useRouter();
-  const { redirectUrl } = useLogin();
-
-  const onSuccess = async (credentialResponse: CredentialResponse) => {
-    console.log('credentialResponse', credentialResponse);
-    const id_token = credentialResponse?.credential;
-    const client_id = credentialResponse?.clientId;
-
-    if (!id_token || !client_id) {
-      console.error('ID Token not found:', credentialResponse);
-      return;
-    }
-
-    const res = await googleLogin(id_token, client_id).catch((error) => {
-      console.error(error);
-    });
-
-    if (!res) {
-      return;
-    }
-
-    setTokenAndMarkInitialized(res.data.jwt_token);
-    mutateFetchProfile();
-    localStorage.removeItem(loginRedirectUrlKey);
-    if (res.data.registered) router.push(redirectUrl === '' ? 'top' : redirectUrl);
-    else router.push('editprofile?registerMode=true');
-  };
-
-  const onError = (error: void) => {
-    console.log('Login Failed:', error);
-    return error;
-  };
-
-  const [width, setWidth] = useState(0);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (!googleRegister) {
-        setWidth(window.innerWidth > 1024 ? 390 : 300);
-      } else {
-        setWidth(window.innerWidth > 1024 ? 310 : 300);
-      }
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  return <GoogleLogin onSuccess={onSuccess} onError={onError} type="standard" logo_alignment="left" width={width} />;
-};
-
-const GoogleLoginButton = ({ googleRegister }: GoogleRegisterProps) => {
-  const clientId = process.env.GOOGLE_CLIENT_ID || '';
+const GoogleLoginButton = (props: GoogleRegisterProps) => {
+  const { googleRegister } = props;
+  const { clientId, onSuccess, onError, width } = useGoogleLoginButton({ googleRegister });
 
   return (
     <div className="group flex justify-center">
-      <div className="absolute z-50 mt-2 opacity-0 group-hover:bg-monotone-200">
+      <div className="opacity-1 absolute z-50 mt-2 group-hover:bg-monotone-200">
         <GoogleOAuthProvider clientId={clientId}>
-          <LoginButton googleRegister={googleRegister} />
+          <GoogleLogin onSuccess={onSuccess} onError={onError} type="standard" logo_alignment="left" width={width} />
         </GoogleOAuthProvider>
       </div>
       <div className="absolute m-auto cursor-pointer rounded-md border border-border-field group-hover:bg-monotone-200">
--- src/hooks/api/doctor/useGoogleLogin.ts
+++ src/hooks/api/doctor/useGoogleLogin.ts
@@ -1,21 +1,22 @@
 import { useAxios } from '@/hooks/network/useAxios';
 import { useCallback } from 'react';
 
-export type PostGoogleLoginResponseData = {
+type PostGoogleLoginResponseData = {
   jwt_token: string;
   registered: boolean;
   message?: string;
 };
 
+type PostGoogleLoginRequestData = {
+  id_token: string;
+  queries: Record<string, string>;
+};
+
 export const useGoogleLogin = () => {
   const { axios } = useAxios();
-
   const googleLogin = useCallback(
-    (id_token: string, client_id: string) => {
-      return axios.post<PostGoogleLoginResponseData>('/oauth/google/verify-token', {
-        id_token: id_token,
-        client_id: client_id,
-      });
+    (data: PostGoogleLoginRequestData) => {
+      return axios.post<PostGoogleLoginResponseData>('/oauth/google/verify-token', data);
     },
     [axios]
   );
